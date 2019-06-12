8737e04e439a0de0ac950b280213684856c4deea
pelssers <bartpelssers@gmail.com>
2019-06-12 17:33:40 +0200

    Pass ndarray instead of pandas Series to tensor

--- flamedisx/core.py
+++ flamedisx/core.py
@@ -364,9 +364,9 @@ class ERSource:
 
         # Precompute tensors for use in gimme
         for fname, v in self.f_dims.items():
-            self.tensor_data[fname] = [tf.convert_to_tensor(d[x], dtype=tf.float32) for x in v]
+            self.tensor_data[fname] = [tf.convert_to_tensor(d[x].values, dtype=tf.float32) for x in v]
         for fname in ['s1', 's2']:
-            self.tensor_data[fname] = tf.convert_to_tensor(d[fname], dtype=tf.float32)
+            self.tensor_data[fname] = tf.convert_to_tensor(d[fname].values, dtype=tf.float32)
 
     def likelihood(self, data=None, max_sigma=3, **params):
         self._params = params
