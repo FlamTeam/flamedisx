3af52fb7021c0ce5295d4a01743ea9b71929f4e2
Vasile Cristian Antochi <vasilecristianantochi@eduroam-10-200-62-16.local.su.se>
2019-06-13 16:56:03 +0200

    Commented the reset of data_tensor

--- flamedisx/core.py
+++ flamedisx/core.py
@@ -557,7 +557,7 @@ class ERSource:
         if data is None:
             data = self.data
 
-        self.tensor_data = dict()
+        #self.tensor_data = dict()
         self.set_data(data, **params)# is this necessary? Yes if we give something position dependent
         
         if isinstance(energies, (float, int)):
