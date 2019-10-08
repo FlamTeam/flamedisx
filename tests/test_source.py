afc22c39b55ae69f4c6123c19089de46d3ab18d7
pelssers <bartpelssers@gmail.com>
2019-10-08 17:04:11 +0200

    remove test case

--- tests/test_source.py
+++ tests/test_source.py
@@ -92,16 +92,10 @@ def test_gimme(xes: fd.ERSource):
 def test_simulate(xes: fd.ERSource):
     """Test the simulator with and without fix_truth"""
     n_ev = int(1e3)
-    es = np.linspace(0., 100., n_ev)
 
     # Test simulate with number of events
     simd = xes.simulate(n_ev)
     assert len(simd) <= n_ev
-
-    # Test simulate with list of energies
-    simd = xes.simulate(energies=es)
-    # We can't test if simd['energy'] is equal to input
-    # since events are lost due selection/detection efficiencies
 
     # Test simulate with fix_truth DataFrame
     fix_truth_df = simd.iloc[:1].copy()
