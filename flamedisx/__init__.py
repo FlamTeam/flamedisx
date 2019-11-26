b28b7bd0ed3ebf9f14fd06b7117fd207c9c39fd9
pelssers <bartpelssers@gmail.com>
2019-11-26 15:12:54 +0100

    Bump version: 0.3.0 → 0.3.1

--- flamedisx/__init__.py
+++ flamedisx/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 from .utils import *
 from .xenon_corrections import *
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('HISTORY.md') as file:
 
 setuptools.setup(
     name='flamedisx',
-    version='0.3.0',
+    version='0.3.1',
     description='Fast likelihood analysis in more dimensions for xenon TPCs',
     author='Jelle Aalbers, Bart Pelssers, Cristian Antochi',
     url='https://github.com/JelleAalbers/flamedisx',
