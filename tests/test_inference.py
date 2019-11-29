15aeb01c894baef5df7cf8082ba2a987f44115f7
Jelle Aalbers <jelleaalbers@gmail.com>
2019-11-29 19:29:46 +0100

    Fix tests

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -1,30 +1,10 @@
 import numpy as np
-import pandas as pd
-import pytest
-import tensorflow as tf
-import tensorflow_probability as tfp
-from iminuit import Minuit
 
 import flamedisx as fd
-from flamedisx.likelihood import DEFAULT_DSETNAME
+from test_source import xes   # Yes, it is used through pytest magic
 
 
 n_events = 2
-
-@pytest.fixture(params=["ER", "NR"])
-def xes(request):
-    # warnings.filterwarnings("error")
-    data = pd.DataFrame([dict(s1=56., s2=2905., drift_time=143465.,
-                              x=2., y=0.4, z=-20, r=2.1, theta=0.1,
-                              event_time=15e17),
-                         dict(s1=23, s2=1080., drift_time=445622.,
-                              x=1.12, y=0.35, z=-59., r=1., theta=0.3,
-                              event_time=15e17)])
-    if request.param == 'ER':
-        x = fd.ERSource(data.copy(), batch_size=2, max_sigma=8)
-    else:
-        x = fd.NRSource(data.copy(), batch_size=2, max_sigma=8)
-    return x
 
 
 def test_one_parameter_interval(xes):
--- tests/test_mock_inference.py
+++ tests/test_mock_inference.py
@@ -1,128 +1,70 @@
 import numpy as np
-import pandas as pd
 import pytest
 import tensorflow as tf
-import tensorflow_probability as tfp
-from iminuit import Minuit
 
 import flamedisx as fd
-from flamedisx.likelihood import DEFAULT_DSETNAME
-from flamedisx.inference import (ScipyObjective,
-                                 TensorFlowObjective,
-                                 MinuitObjective)
-
 
 n_events = 2
 
-class MockObjective:
-    def _inner_fun_and_grad(self, params):
-        # Return -2lnL and -2lnL gradient
-        # Use simple parabola with NaN bounds for rate multipliers
-        # Set true parameters in self.truths[param_name] before
-        ll = 0
-        grads_to_return = [p for p in params if p not in self.fix.keys()]
-        n_grads = len(grads_to_return)
-        ll_grad_dict = {k: 0 for k in grads_to_return}
-
-        for k, v in params.items():
-            v = v.numpy()
-            if k.endswith('_rate_multiplier') and v <= 0.:
-                ll = float('nan')
-                ll_grad = [float('nan')] * n_grads
-                break
-            # Add parabola
-            ll += (v - self.truths[k]) ** 2
-            ll_grad_dict[k] += 2 * (v - self.truths[k])
-        return (tf.constant(ll, dtype=fd.float_type()),
-                tf.constant(list(ll_grad_dict.values()), dtype=fd.float_type()))
+arg_names = ['er_rate_multplier', 'elife']
+truth_test = np.array([2., 3.])
 
 
-class MockScipyObjective(MockObjective, ScipyObjective):
-    pass
+@pytest.fixture(params=fd.BESTFIT_OBJECTIVES.values())
+def mock_objective(request):
+
+    class MockObjective(request.param):
+        def __init__(self, *, truths, **kwargs):
+            self.arg_names = list(truths.keys())
+            self.truths = truths
+            super().__init__(**kwargs)
+
+        def _inner_fun_and_grad(self, params):
+            # Return -2lnL and -2lnL gradient
+            # Use simple parabola with NaN bounds for rate multipliers
+            # Set true parameters in self.truths[param_name] before
+            ll = 0
+            grads_to_return = [p for p in params if p not in self.fix.keys()]
+            n_grads = len(grads_to_return)
+            ll_grad_dict = {k: 0 for k in grads_to_return}
+
+            for k, v in params.items():
+                # TODO: investigate and remove hack
+                try:
+                    v = v.numpy()
+                except AttributeError:
+                    pass
+
+                if k.endswith('_rate_multiplier') and v <= 0.:
+                    ll = float('nan')
+                    ll_grad = [float('nan')] * n_grads
+                    break
+                # Add parabola
+                ll += (v - self.truths[k]) ** 2
+                ll_grad_dict[k] += 2 * (v - self.truths[k])
+            return (tf.constant(ll, dtype=fd.float_type()),
+                    tf.constant(list(ll_grad_dict.values()), dtype=fd.float_type()))
+
+    truths = {k: truth_test[i] for i, k in enumerate(arg_names)}
+    guess = {k: 1. for k in arg_names}
+
+    return MockObjective(
+        lf=None,
+        truths=truths,
+        guess=guess,
+        fix=dict(),
+        llr_tolerance=0.001,
+        nan_val=float('nan'),
+        get_lowlevel_result=False,
+        # We haven't defined a mock grad2
+        use_hessian=False)
 
 
-class MockTensorFlowObjective(MockObjective, TensorFlowObjective):
-    pass
-
-
-class MockMinuitObjective(MockObjective, MinuitObjective):
-    pass
-
-
-def test_mock_bestfit_tf():
+def test_mock_bestfit_tf(mock_objective):
     # Test bestfit (including hessian)
-    arg_names = ['er_rate_multiplier', 'elife']
-    mock_opt = MockTensorFlowObjective(lf=None,
-                                       arg_names=arg_names,
-                                       fix=dict(),
-                                       autograph=True,
-                                       nan_val=float('nan'))
-
-    truths = dict(er_rate_multiplier=2.,
-                  elife=2.)
-    truth_test = np.array([truths[k] for k in arg_names])
-    mock_opt.truths = truths
-    x_guess = np.array([1., 1.])  # optmizer start
-
-    res = mock_opt.minimize(x_guess,
-                            get_lowlevel_result=False,
-                            # We haven't defined a mock grad2
-                            use_hessian=False,
-                            llr_tolerance=0.1)
-
-    res_test = np.array([res[k] for k in arg_names])
-
-    np.testing.assert_array_almost_equal(truth_test,
-                                         res_test, decimal=3)
-
-
-def test_mock_bestfit_minuit():
-    # Test bestfit (including hessian)
-    arg_names = ['er_rate_multiplier', 'elife']
-    mock_opt = MockMinuitObjective(lf=None,
-                                   arg_names=arg_names,
-                                   fix=dict(),
-                                   autograph=True,
-                                   nan_val=float('nan'))
-
-    truths = dict(er_rate_multiplier=2.,
-                  elife=2.)
-    truth_test = np.array([truths[k] for k in arg_names])
-    mock_opt.truths = truths
-    x_guess = np.array([1., 1.])
-
-    res = mock_opt.minimize(x_guess,
-                            get_lowlevel_result=False,
-                            use_hessian=True,
-                            llr_tolerance=0.001)
-
-    res_test = np.array([res[k] for k in arg_names])
-
-    np.testing.assert_array_almost_equal(truth_test,
-                                         res_test, decimal=3)
-
-
-def test_mock_bestfit_scipy():
-    # Test bestfit (including hessian)
-    arg_names = ['er_rate_multiplier', 'elife']
-    mock_opt = MockScipyObjective(lf=None,
-                                  arg_names=arg_names,
-                                  fix=dict(),
-                                  autograph=True,
-                                  nan_val=float('nan'))
-
-    truths = dict(er_rate_multiplier=2.,
-                  elife=2.)
-    truth_test = np.array([truths[k] for k in arg_names])
-    mock_opt.truths = truths
-    x_guess = np.array([1., 1.])
-
-    res = mock_opt.minimize(x_guess,
-                            get_lowlevel_result=False,
-                            use_hessian=True,
-                            llr_tolerance=0.1)
-
-    res_test = np.array([res[k] for k in arg_names])
+    res = mock_objective.minimize()
+    res_test = np.array([res[k]
+                         for k in mock_objective.arg_names])
 
     np.testing.assert_array_almost_equal(truth_test,
                                          res_test, decimal=3)
