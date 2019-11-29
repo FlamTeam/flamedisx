31e0e09c7995fe41dacf340035c27555e63cc0ed
Jelle Aalbers <jelleaalbers@gmail.com>
2019-11-29 14:37:51 +0100

    Oops

--- flamedisx/utils.py
+++ flamedisx/utils.py
@@ -195,4 +195,4 @@ def values_to_constants(kwargs):
     for k, v in kwargs.items():
         if isinstance(v, (float, int)) or is_numpy_number(v):
             kwargs[k] = tf.constant(v, dtype=float_type())
-
+    return kwargs
