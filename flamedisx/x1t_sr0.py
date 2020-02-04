0dc294174aa28417793f5f3c0cc31abea3fbfec0
Jelle Aalbers <JelleAalbers@users.noreply.github.com>
2020-02-04 10:44:09 +0100

    Make mean_eff a parameter

--- flamedisx/x1t_sr0.py
+++ flamedisx/x1t_sr0.py
@@ -100,9 +100,10 @@ class SR0Source:
         return mean_eff * s1_relative_ly
 
     @staticmethod
-    def s1_acceptance(s1, photon_detection_eff, photon_gain_mean):
+    def s1_acceptance(s1, photon_detection_eff, photon_gain_mean,
+                      mean_eff=0.142 / (1 + 0.219)):
         # Both cS1 and S1 acceptance
-        cs1 = (0.142 / (1 + 0.219)) * s1 / (
+        cs1 = mean_eff * s1 / (
             photon_detection_eff * photon_gain_mean)
         return tf.where((s1 < 2) | (s1 > 70) | (cs1 < 2),
                         tf.zeros_like(s1, dtype=fd.float_type()),
