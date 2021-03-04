187ada41a52f09992544b267bd36b30475ced92b
Jelle Aalbers <jelleaalbers@gmail.com>
2021-03-04 15:51:21 -0800

    set_temporarily did not actually reset defaults

--- flamedisx/source.py
+++ flamedisx/source.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from contextlib import contextmanager
 import inspect
 
@@ -255,7 +256,7 @@ class Source:
         data tensor state"""
         if data is None:
             raise ValueError("No point in setting data = None temporarily")
-        old_defaults = self.defaults
+        old_defaults = deepcopy(self.defaults)
         if data is None:
             self.set_defaults(**kwargs)
         else:
