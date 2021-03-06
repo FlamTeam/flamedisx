66a6b3539fea0f2d9ea27722182e5342a98bf043
Jelle Aalbers <jelleaalbers@gmail.com>
2021-03-05 19:15:00 -0800

    Bump version: 1.3.0 → 1.4.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('HISTORY.md') as file:
 
 setuptools.setup(
     name='flamedisx',
-    version='1.3.0',
+    version='1.4.0',
     description='Fast likelihood analysis in more dimensions for xenon TPCs',
     author='Jelle Aalbers, Bart Pelssers, Cristian Antochi',
     url='https://github.com/FlamTeam/flamedisx',
