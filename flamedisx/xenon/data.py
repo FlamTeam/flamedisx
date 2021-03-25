12af3a9d8bbbf29782c7d40cd29c7aacc346972b
Pueh Leng Tan <puehleng.tan@gmail.com>
2021-03-25 11:51:09 +0100

    Added functions to pull data from private xenon nt repo

--- flamedisx/xenon/data.py
+++ flamedisx/xenon/data.py
@@ -48,3 +48,34 @@ def get_bbf_file(data_file_name):
     """
     ensure_bbf()
     return fd.get_resource(f'{BBF_PATH}/bbf/data/{data_file_name}')
+
+
+@export
+def ensure_nt(token=None):
+    """Clones XENONnT/Flamedisx (prompting for credentials) if we do not have it"""
+    if not os.path.exists(NTFD_PATH):
+        print("XENONnT private data requested, we must clone Flamedisx folder (~xx MB).")
+        if token is None:
+            print("    - Create a token with full 'repo' permissions at "
+                  "https://github.com/settings/tokens\n"
+                  "    - Save or write it down somewhere \n"
+                  "    - Type it in the prompt above\n\n"
+                  "'Repository not found' means you didn't give the token"
+                  " full 'repo' permissions.\n"
+                  "'Authentication failed' means you mistyped the token.\n")
+            # We could prompt for username/password instead, but then the password
+            # would be printed in plaintext if there is any problem during cloning.
+            token = getpass.getpass('Github OAuth token: ')
+        fd.run_command(f'git clone https://{token}:x-oauth-basic'
+                       f'@github.com/XENONnT/Flamedisx.git {NTFD_PATH}')
+
+
+@export
+def get_nt_file(data_file_name):
+    """Return information from file in XENONnT/Flamedisx/...
+
+    Do NOT call on import time --
+    that would make flamedisx unusable to non-XENON folks!
+    """
+    ensure_nt()
+    return fd.get_resource(f'{NTFD_PATH}/{data_file_name}')
