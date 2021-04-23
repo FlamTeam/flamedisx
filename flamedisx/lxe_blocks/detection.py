Fix issue with quanta tensor indexing, hacky way to fix up some other dimensions-related issues. Average correctly (I think?) over the full quanta tensor. Still not working though...

flamedisx/lxe_blocks/detection.py
@@ -89,10 +89,10 @@ class DetectPhotonsOrElectrons(fd.Block):
             d[self.quanta_name + 's_produced_' + bound] = intify(
                 n_prod_mle + sign * self.source.max_sigma * _std
             ).clip(0, None).astype(np.int)
-        d['electrons_produced_min'] = 1200
-        d['electrons_produced_max'] = 3200
-        d['photons_produced_min'] = 4000
-        d['photons_produced_max'] = 6000
+        d['electrons_produced_min'] = 500
+        d['electrons_produced_max'] = 1500
+        d['photons_produced_min'] = 2100
+        d['photons_produced_max'] = 3100
 
 
 @export

flamedisx/lxe_blocks/quanta_generation.py
@@ -40,16 +40,17 @@ class MakeERQuanta(fd.Block):
         result = tf.cast(tf.equal(quanta_produced_noStep,
         quanta_produced_real), dtype=fd.float_type())
 
-        # Chunks to sum result in, to get to same dimension as
+        # Chunks to average result in, to get to same dimension as
         # quanta_produced
         chunks = int(result.shape[1] / quanta_produced.shape[1])
 
-        # Perform the summing - will later multiply by the stepping, so divide
+        # Take the average - will later multiply by the stepping, so divide
         # out by it here
         result_temp = tf.reshape(result, [result.shape[0],
         int(result.shape[1] / chunks), chunks, result.shape[2]])
         result = tf.reduce_sum(result_temp, axis=2) \
-        / self.source.steps['quanta_produced'][:,:,o]
+        / (self.source.steps['quanta_produced'][:, : ,o] \
+        * self.source.steps['quanta_produced'][:, : ,o])
 
         return result
 

flamedisx/lxe_blocks/quanta_splitting.py
@@ -37,8 +37,9 @@ class MakePhotonsElectronsBinomial(fd.Block):
         # ... numbers of total quanta produced
         nq = electrons_produced + photons_produced
         # ... indices in nq arrays
-        _nq_ind = nq - self.source._fetch(
-            'quanta_produced_min', data_tensor=data_tensor)[:, o, o]
+        _nq_ind = tf.round((nq - self.source._fetch(
+            'quanta_produced_min', data_tensor=data_tensor)[:, o, o]) \
+        / self.source.steps['quanta_produced'][:, :, o])
         # ... differential rate
         rate_nq = fd.lookup_axis1(rate_vs_quanta, _nq_ind)
         # ... probability of a quantum to become an electron

flamedisx/source.py
@@ -85,7 +85,7 @@ class Source:
                  data=None,
                  batch_size=10,
                  max_sigma=3,
-                 max_dim_size=100,
+                 max_dim_size=70,
                  data_is_annotated=False,
                  _skip_tf_init=False,
                  _skip_bounds_computation=False,
@@ -263,12 +263,11 @@ class Source:
             self.dimsizes[dim] = int(tf.reduce_max(ma - mi + 1).numpy())
             if (self.dimsizes[dim] > max_dim_size) and (dim not in self.no_step_dimensions):
                 self.dimsizes[dim] = max_dim_size
+            if dim=='quanta_produced':
+                self.dimsizes[dim] = 70 + 70 - 1
         for dim in self.final_dimensions:
             self.dimsizes[dim] = 1
-        for dim in self.bonus_dimensions:
-            ma = self._fetch(dim + '_max')
-            mi = self._fetch(dim + '_min')
-            self.dimsizes[dim] = int(tf.reduce_max(ma - mi + 1).numpy())
+        self.dimsizes['quanta_produced_noStep'] = self.dimsizes['quanta_produced'] + int(tf.reduce_max((tf.convert_to_tensor(15 * np.ones((10,1)), dtype=fd.float_type()) - 1)).numpy()) * (self.dimsizes['quanta_produced'] - 1)
 
     @contextmanager
     def _set_temporarily(self, data, **kwargs):
@@ -456,8 +455,12 @@ class Source:
                          1) # We want to ceil this to ensure we cover to at least the upper bound
         # Store the steps for later multiplying probabilities
         self.steps[x] = steps
+
+        if x=='quanta_produced':
+            self.steps[x] = tf.convert_to_tensor(15 * np.ones((10,1)), dtype=fd.float_type())
+
         # Cover the bounds range in integer steps not necessarily of 1
-        x_range = tf.cast(tf.range(self.dimsizes[x]), dtype=fd.float_type()) * steps
+        x_range = tf.cast(tf.range(self.dimsizes[x]), dtype=fd.float_type()) * self.steps[x]
         return left_bound + x_range
 
     def cross_domains(self, x, y, data_tensor):