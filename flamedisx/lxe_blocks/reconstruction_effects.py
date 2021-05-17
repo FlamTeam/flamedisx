f643e55c367b48a851d1a1445075aea4972233e6
Pueh Leng Tan <puehleng.tan@gmail.com>
2021-05-17 19:02:04 +0200

    attempt to calculate some bound in _annotate

--- flamedisx/lxe_blocks/reconstruction_effects.py
+++ flamedisx/lxe_blocks/reconstruction_effects.py
@@ -10,6 +10,7 @@ export, __all__ = fd.exporter()
 o = tf.newaxis
 
 
+import pdb as pdb
 class ReconstructSignals(fd.Block):
     """Common code for ReconstructS1 and ReconstructS2"""
     model_attributes = ('check_acceptances',)
@@ -35,8 +36,11 @@ class ReconstructSignals(fd.Block):
         d['p_accepted'] *= self.gimme_numpy(self.signal_name + '_acceptance')
 
     def _annotate(self, d):
-        tf.print('PASSED: Dunno what to do here.')
-        pass
+        for bound, intify in (('_min', np.min),
+                              ('_max', np.max)):
+            # Dunno how to calculate
+            d[self.signal_name + bound] = intify(
+                    d[self.signal_name]).clip(0, None).astype(np.int)
     
     def _compute(self,
                  s_observed,
