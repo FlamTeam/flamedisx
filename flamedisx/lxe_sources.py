0c74cf9cb8a55c414da8d53181c49956f5280ea5
Robert James <robert.james.19@ucl.ac.uk>
2021-09-23 15:55:54 +0200

    Allow for penultimate dimensions to have different max_dim_size, have default option for max_dim_size and max_dim_size_outer.

--- flamedisx/lxe_sources.py
+++ flamedisx/lxe_sources.py
@@ -33,6 +33,8 @@ class ERSource(fd.BlockModelSource):
         return fd.safe_p(qy * 13.7e-3)
 
     final_dimensions = ('s1', 's2')
+    penultimate_dimensions = ('photoelectrons_detected',
+                              'electrons_detected')
     no_step_dimensions = ()
 
 
@@ -49,6 +51,8 @@ class NRSource(fd.BlockModelSource):
         fd.MakeS2)
 
     final_dimensions = ('s1', 's2')
+    penultimate_dimensions = ('photoelectrons_detected',
+                              'electrons_detected')
     no_step_dimensions = ()
 
     # Use a larger default energy range, since most energy is lost
