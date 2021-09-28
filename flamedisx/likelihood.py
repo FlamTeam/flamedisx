Fix hessian bug (#176)
* bug fix to initial guess of sigma for interval computation

* fixed computation of covariance from inverse hessian

* changed variable name for clarity

flamedisx/likelihood.py
@@ -740,7 +740,7 @@ class LogLikelihood:
         return np.linalg.inv(-2 * grad2_ll)
 
     def summary(self, bestfit=None, fix=None, guess=None,
-                inverse_hessian=None, precision=3):
+                cov=None, precision=3):
         """Print summary information about best fit"""
         if fix is None:
             fix = dict()
@@ -748,12 +748,15 @@ class LogLikelihood:
             bestfit = self.bestfit(guess=guess, fix=fix)
 
         params = {**bestfit, **fix}
-        if inverse_hessian is None:
-            inverse_hessian = self.inverse_hessian(
+        if cov is None:
+            # inverse_hessian returns inverse of hessian of -2 loglikelihood
+            # but covariance matrix is inverse of hessian of - logikelihood
+            # (kA)^(-1) = k^(-1)A^(-1) for non-zero scalar k
+            cov = 2. * self.inverse_hessian(
                 params,
                 omit_grads=tuple(fix.keys()))
 
-        stderr, cov = cov_to_std(inverse_hessian)
+        stderr, corr = cov_to_std(cov)
 
         var_par_i = 0
         for i, pname in enumerate(self.param_names):
@@ -771,7 +774,7 @@ class LogLikelihood:
 
         var_pars = [x for x in self.param_names if x not in fix]
         df = pd.DataFrame(
-            {p1: {p2: cov[i1, i2]
+            {p1: {p2: corr[i1, i2]
                   for i2, p2 in enumerate(var_pars)}
              for i1, p1 in enumerate(var_pars)},
             columns=var_pars)