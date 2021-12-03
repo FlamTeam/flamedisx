e125c47e1c50803685aae33f1326f1418817b6a4
Robert James <robert.james.19@ucl.ac.uk>
2021-12-03 13:58:16 +0000

    Acutally finish thing for memory profiling...

--- flamedisx/nest/lxe_blocks/quanta_splitting.py
+++ flamedisx/nest/lxe_blocks/quanta_splitting.py
@@ -43,17 +43,19 @@ class MakePhotonsElectronsNR(fd.Block):
             rate_vs_energy = args[1]
             energy_index = args[2]
 
-            ions_min_initial = self.ion_bounds_min_tensor[i_batch, :, 0, o]
-            ions_min_initial = tf.repeat(ions_min_initial, tf.shape(ions_produced)[1], axis=1)
-            ions_min_initial = tf.repeat(ions_min_initial[:, :, o], tf.shape(ions_produced)[2], axis=2)
-            ions_min_initial = tf.repeat(ions_min_initial[:, :, :, o], tf.shape(ions_produced)[3], axis=3)
+            # ions_min_initial = self.ion_bounds_min_tensor[i_batch, :, 0, o]
+            # ions_min_initial = tf.repeat(ions_min_initial, tf.shape(ions_produced)[1], axis=1)
+            # ions_min_initial = tf.repeat(ions_min_initial[:, :, o], tf.shape(ions_produced)[2], axis=2)
+            # ions_min_initial = tf.repeat(ions_min_initial[:, :, :, o], tf.shape(ions_produced)[3], axis=3)
+            #
+            # ions_min = self.ion_bounds_min_tensor[i_batch, :, energy_index, o]
+            # ions_min = tf.repeat(ions_min, tf.shape(ions_produced)[1], axis=1)
+            # ions_min = tf.repeat(ions_min[:, :, o], tf.shape(ions_produced)[2], axis=2)
+            # ions_min = tf.repeat(ions_min[:, :, :, o], tf.shape(ions_produced)[3], axis=3)
+            #
+            # _ions_produced = ions_produced - ions_min_initial + ions_min
 
-            ions_min = self.ion_bounds_min_tensor[i_batch, :, energy_index, o]
-            ions_min = tf.repeat(ions_min, tf.shape(ions_produced)[1], axis=1)
-            ions_min = tf.repeat(ions_min[:, :, o], tf.shape(ions_produced)[2], axis=2)
-            ions_min = tf.repeat(ions_min[:, :, :, o], tf.shape(ions_produced)[3], axis=3)
-
-            _ions_produced = ions_produced - ions_min_initial + ions_min
+            _ions_produced = ions_produced
 
             if self.is_ER:
                 nel_mean = self.gimme('mean_yield_electron', data_tensor=data_tensor, ptensor=ptensor,
