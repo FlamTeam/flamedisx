ab0ca6fa4e24578c63692bd956069c79d5fd324a
Robert James <robert.james.19@ucl.ac.uk>
2021-12-07 17:37:36 +0000

    Improve scaling of ion bounds with energy steps.

--- flamedisx/nest/lxe_blocks/quanta_splitting.py
+++ flamedisx/nest/lxe_blocks/quanta_splitting.py
@@ -206,6 +206,46 @@ class MakePhotonsElectronsNR(fd.Block):
                                          ph_prod_temp)
 
     def _annotate_special(self, d):
+        def get_bounds_ER(energy):
+            nel = self.gimme_numpy('mean_yield_electron', energy)
+            nq = self.gimme_numpy('mean_yield_quanta', (energy, nel))
+            fano = self.gimme_numpy('fano_factor', nq)
+            nq_actual_upper = nq + np.sqrt(fano * nq) * self.source.max_sigma
+            nq_actual_lower = nq - np.sqrt(fano * nq) * self.source.max_sigma
+
+            ex_ratio = self.gimme_numpy('exciton_ratio', energy)
+            alpha = 1. / (1. + ex_ratio)
+
+            ions_mean_upper = nq_actual_upper * alpha
+            ions_mean_lower = nq_actual_lower * alpha
+            ions_std_upper = np.sqrt(nq_actual_upper * alpha * (1 - alpha))
+            ions_std_lower = np.sqrt(nq_actual_lower * alpha * (1 - alpha))
+
+            ions_produced_min = np.floor(ions_mean_lower - self.source.max_sigma * ions_std_lower).astype(int)
+            ions_produced_max = np.ceil(ions_mean_upper + self.source.max_sigma * ions_std_upper).astype(int)
+
+            return (ions_produced_min, ions_produced_max)
+
+        def get_bounds_NR(energy):
+            nq = self.gimme_numpy('mean_yields', energy)[1]
+            ex_ratio = self.gimme_numpy('mean_yields', energy)[2]
+            alpha = 1. / (1. + ex_ratio)
+            ions_mean = nq * alpha
+            ions_std = np.sqrt(nq * alpha)
+
+            ions_produced_min = np.floor(ions_mean - self.source.max_sigma * ions_std).astype(int)
+            ions_produced_max = np.ceil(ions_mean + self.source.max_sigma * ions_std).astype(int)
+
+            return (ions_produced_min, ions_produced_max)
+
+        if self.is_ER:
+            bounds = [get_bounds_ER(energy) for energy in self.source.energies.numpy()]
+        else:
+            bounds = [get_bounds_ER(energy) for energy in self.source.energies.numpy()]
+
+        ions_produced_min_full = [x[0] for x in bounds]
+        ions_produced_max_full = [x[1] for x in bounds]
+
         for batch in range(self.source.n_batches):
             d_batch = d[batch * self.source.batch_size : (batch + 1) * self.source.batch_size]
 
@@ -217,38 +257,9 @@ class MakePhotonsElectronsNR(fd.Block):
 
             index_step = np.round(np.linspace(0, len(energies_trim) - 1,
                                               min(len(energies_trim), self.source.max_dim_size_initial))).astype(int)
-            energies_trim_step = energies_trim[index_step]
 
-            ions_produced_min = []
-            ions_produced_max = []
-
-            for energy in energies_trim_step:
-
-                if self.is_ER:
-                    nel = self.gimme_numpy('mean_yield_electron', energy)
-                    nq = self.gimme_numpy('mean_yield_quanta', (energy, nel))
-                    fano = self.gimme_numpy('fano_factor', nq)
-                    nq_actual_upper = nq + np.sqrt(fano * nq) * self.source.max_sigma
-                    nq_actual_lower = nq - np.sqrt(fano * nq) * self.source.max_sigma
-
-                    ex_ratio = self.gimme_numpy('exciton_ratio', energy)
-                    alpha = 1. / (1. + ex_ratio)
-
-                    ions_mean_upper = nq_actual_upper * alpha
-                    ions_mean_lower = nq_actual_lower * alpha
-                    ions_std_upper = np.sqrt(nq_actual_upper * alpha * (1 - alpha))
-                    ions_std_lower = np.sqrt(nq_actual_lower * alpha * (1 - alpha))
-                else:
-                    nq = self.gimme_numpy('mean_yields', energy)[1]
-                    ex_ratio = self.gimme_numpy('mean_yields', energy)[2]
-                    alpha = 1. / (1. + ex_ratio)
-                    ions_mean_upper = nq * alpha
-                    ions_mean_lower = nq * alpha
-                    ions_std_upper = np.sqrt(nq * alpha)
-                    ions_std_lower = np.sqrt(nq * alpha)
-
-                ions_produced_min.append(np.floor(ions_mean_lower - self.source.max_sigma * ions_std_lower).astype(int))
-                ions_produced_max.append(np.ceil(ions_mean_upper + self.source.max_sigma * ions_std_upper).astype(int))
+            ions_produced_min = list(np.take(ions_produced_min_full, index_step))
+            ions_produced_max = list(np.take(ions_produced_max_full, index_step))
 
             indicies = np.arange(batch * self.source.batch_size, (batch + 1) * self.source.batch_size)
             d.loc[batch * self.source.batch_size : (batch + 1) * self.source.batch_size - 1, 'ions_produced_min'] = \
