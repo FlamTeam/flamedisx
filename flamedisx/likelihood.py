f0aa5aa4bfeaeac8ba1c209948e095a26c33cd41
Robert James <robert.james.19@ucl.ac.uk>
2022-03-18 17:17:29 +0100

    Fixes that were causing CI tests in test_likelihood.py to fail.

--- flamedisx/likelihood.py
+++ flamedisx/likelihood.py
@@ -463,8 +463,8 @@ class LogLikelihood:
 
             col_start, col_stop = self.column_indices[dsetname][source_i]
             dr = s.differential_rate(
-                i_batch,
-                data_tensor[:, col_start:col_stop],
+                i_batch=i_batch,
+                data_tensor=data_tensor[:, col_start:col_stop],
                 # We are already tracing; if we call the traced function here
                 # it breaks the Hessian (it will give NaNs)
                 autograph=False,
