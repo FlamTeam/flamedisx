2d4c58ace44883077a5dc28bfa0380e5ead9352d
Robert James <robert.james.19@ucl.ac.uk>
2022-04-02 23:26:55 +0200

    Re-enable NEST source test, change certain values that weren't sensible before.

--- tests/test_nest.py
+++ tests/test_nest.py
@@ -3,22 +3,22 @@ import pandas as pd
 
 def dummy_data():
     return pd.DataFrame(
-        [dict(s1=43., s2=3574., drift_time=65710.930108,
+        [dict(s1=43., s2=3574., drift_time=65710.928173,
               x=0.82062, y=6.028471, z=44.785994, r=6.084068, theta=1.435504,
               event_time=1579784955000000000),
-         dict(s1=45., s2=2308., drift_time=55682.601147,
-              x=-13.22201, y=14.981184, z=-46.265423, r=19.981427, theta=2.293900,
+         dict(s1=45., s2=2308., drift_time=276544.954682,
+              x=-13.22201, y=14.981184, z=13.6827, r=19.981427, theta=2.293900,
               event_time=1579784956000000000)])
 
 
 def test_nest_source():
     # Just import and initialize without data
     import flamedisx.nest as fd_nest
-    fd_nest.nestERSource()
+    fd_nest.nestERSource(energy_min=8, energy_max=8, num_energies=1)
 
     # Initialize with data
     df_test = dummy_data()
-    s = fd_nest.nestERSource(df_test, batch_size=2)
+    s = fd_nest.nestERSource(df_test, energy_min=8, energy_max=8, num_energies=1, batch_size=2)
 
     # Simulate events
     d_sim = s.simulate(1000)
@@ -28,21 +28,18 @@ def test_nest_source():
     # Mu estimation (based on simulation)
     s.estimate_mu()
 
-    # Commenting out the below for now, as it relies on the TFP files being in
-    # the right place...
+    # Differential rate
+    dr = s.differential_rate(data_tensor=s.data_tensor[0])
+    assert len(dr) == len(df_test)
+    assert min(dr) > 0
 
-    # # Differential rate
-    # dr = s.differential_rate(s.data_tensor[0])
-    # assert len(dr) == len(df_test)
-    # assert min(dr) > 0
-    #
-    # # NEST sources have been checked to match NEST.
-    # # If there are any changes that cause them to output different values,
-    # # we have to check they still match.
-    # # This test prevents any such changes from passing unless someone
-    # # manually updates the values below.
-    # np.testing.assert_allclose(
-    #     dr.numpy(),
-    #     [0.00287317, 0.00692382],
-    #     # For some reason, we get different values on different machines
-    #     rtol=5e-3)
+    # NEST sources have been checked to match NEST.
+    # If there are any changes that cause them to output different values,
+    # we have to check they still match.
+    # This test prevents any such changes from passing unless someone
+    # manually updates the values below.
+    np.testing.assert_allclose(
+        dr.numpy(),
+        [1.9269697e-05, 4.2966261e-05],
+        # For some reason, we get different values on different machines
+        rtol=5e-3)
