d1626adc1c6a4951753d518b6acaf4d4f6264b53
Robert James <robert.james.19@ucl.ac.uk>
2022-08-12 16:09:03 +0200

    Deprecate extraction efficiency and electron yield calculation for nT; use Pueh's numbers instead. Adjust g1_gas for first pass accordingly. Also, add in some of Matthew's cuts.

--- flamedisx/xenon_nT_MS/nT.py
+++ flamedisx/xenon_nT_MS/nT.py
@@ -1,6 +1,8 @@
 """Matthew Syzdagis' mock-up of the XENONnT detector implementation
 
 """
+import tensorflow as tf
+
 import configparser
 import os
 
@@ -8,6 +10,7 @@ import flamedisx as fd
 from .. import nest as fd_nest
 
 export, __all__ = fd.exporter()
+o = tf.newaxis
 
 
 ##
@@ -38,6 +41,18 @@ class XENONnTSource:
          config.getfloat('NEST', 'temperature_config')).item()
 
         super().__init__(*args, **kwargs)
+
+        self.extraction_eff = 0.52
+
+    def electron_gain_mean(self):
+        elYield = 31.2
+
+        return tf.cast(elYield, fd.float_type())[o]
+
+    def electron_gain_std(self):
+        elYield = 31.2
+
+        return tf.sqrt(self.s2Fano * elYield)[o]
 
 
 @export
