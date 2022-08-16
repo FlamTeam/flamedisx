Typcasting fix, account for NR ions produced Fano factor in bounds calculation.

flamedisx/nest/lxe_blocks/quanta_splitting.py
@@ -305,8 +305,10 @@ class MakePhotonsElectronsNR(fd.Block):
             nq = self.gimme_numpy('mean_yields', energy)[1]
             ex_ratio = self.gimme_numpy('mean_yields', energy)[2]
             alpha = 1. / (1. + ex_ratio)
+            ni_fano = self.gimme_numpy('yield_fano', nq)[0]
+
             ions_mean = nq * alpha
-            ions_std = np.sqrt(nq * alpha)
+            ions_std = np.sqrt(nq * alpha * ni_fano)
 
             ions_produced_min = np.floor(ions_mean - self.source.max_sigma * ions_std).astype(int)
             ions_produced_max = np.ceil(ions_mean + self.source.max_sigma * ions_std).astype(int)

flamedisx/nest/lxe_sources.py
@@ -384,7 +384,7 @@ class nestNRSource(nestSource):
 
         nex = nq - ni
 
-        ex_ratio = nex / ni
+        ex_ratio = tf.cast(nex / ni, fd.float_type())
 
         ex_ratio = tf.where(tf.logical_and(ex_ratio < self.alpha, energy > 100.),
                             self.alpha * tf.ones_like(ex_ratio, dtype=fd.float_type()),