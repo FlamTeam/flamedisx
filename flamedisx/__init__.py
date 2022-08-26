9ec83a959e709ab52c1930297a1271b6718071a3
Robert James <robert.james.19@ucl.ac.uk>
2022-08-26 16:20:59 +0200

    Start of class for getting frequentist upper limits in non-asymptotic regime, where we only want to float rates as NPs.

--- flamedisx/__init__.py
+++ flamedisx/__init__.py
@@ -9,6 +9,7 @@ from .inference import *
 from .bounds import *
 from .mu_estimation import *
 from .frozen_reservoir import *
+from .non_asymptotic_inference import *
 
 # Original flamedisx models
 # Accessible under fd root package (for now), for backwards compatibility
