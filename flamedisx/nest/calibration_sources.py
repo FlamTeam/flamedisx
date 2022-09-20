4cfa82aedff1766ecdd8e122e59b5d576a11a57f
Robert James <robert.james.19@ucl.ac.uk>
2022-09-20 12:09:57 +0100

    Actual calculation of tritium spectrum, as in NEST.

--- flamedisx/nest/calibration_sources.py
+++ flamedisx/nest/calibration_sources.py
@@ -9,6 +9,9 @@ import pandas as pd
 import flamedisx as fd
 from . import lxe_sources as fd_nest
 
+import math as m
+pi = tf.constant(m.pi)
+
 export, __all__ = fd.exporter()
 
 
@@ -24,6 +27,18 @@ class CH3TSource(fd_nest.nestERSource):
             kwargs['detector'] = 'default'
         super().__init__(*args, **kwargs)
 
+        m_e = 510.9989461
+        aa = 0.0072973525664
+        ZZ = 2.
+        qValue = 18.5898
+
+        energies = tf.linspace(0.01, qValue, 1000)
+
+        B = tf.sqrt(energies**2 + 2. * energies * m_e) / (energies + m_e)
+        x = (2. * pi * ZZ * aa) * (energies + m_e) / tf.sqrt(energies**2 + 2. * energies * m_e)
+        spectrum = tf.sqrt(2. * energies * m_e) * (energies + m_e) * (qValue - energies) * (qValue - energies) * x * (1. / (1. - tf.exp(-x))) * (1.002037 - 0.001427 * B)
+        spectrum = spectrum / tf.math.reduce_max(spectrum)
+
         df_tritium = pd.read_pickle(os.path.join(os.path.dirname(__file__), 'calibration_spectra/tritium_spectrum.pkl'))
-        self.energies = tf.convert_to_tensor(df_tritium['energies'].values, dtype=fd.float_type())
-        self.rates_vs_energy = tf.convert_to_tensor(df_tritium['rates_vs_energy'].values, dtype=fd.float_type())
+        self.energies = tf.cast(energies, fd.float_type())
+        self.rates_vs_energy = tf.cast(spectrum, fd.float_type())
