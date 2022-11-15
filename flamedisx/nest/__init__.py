d37d70e7bb0954e116776036f29d440e9749f3e3
Robert James <robert.james.19@ucl.ac.uk>
2022-11-14 21:07:35 -0700

    Begin to add the SourceGroup class.

--- flamedisx/nest/__init__.py
+++ flamedisx/nest/__init__.py
@@ -7,5 +7,8 @@ from .lxe_blocks.detection import *
 from .lxe_blocks.double_pe import *
 from .lxe_blocks.pe_detection import *
 from .lxe_blocks.final_signals import *
+from .lxe_blocks.quanta_splitting_source_group import *
 
 from .lxe_sources import *
+
+from .source_group import *
