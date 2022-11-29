43a911ec4a76baf029a9a7807126a6e0164141ba
Robert James <robert.james.19@ucl.ac.uk>
2022-11-28 17:47:20 -0700

    Scale the diff. rates by a computed energy spectrum from a real source.

--- flamedisx/nest/source_group.py
+++ flamedisx/nest/source_group.py
@@ -57,10 +57,32 @@ class SourceGroup:
         self.base_source.data = self.base_source.data[:self.base_source.n_events]
         self.base_source.data['energies_diff_rates'] = energies_diff_rates_all
 
+    @staticmethod
+    def scale_by_spectrum(energies_diff_rates, spectrum_values):
+        diff_rates = []
+        for es_drs, spectrum in zip(energies_diff_rates, spectrum_values):
+            this_diff_rate = 0.
+            for e_dr, spectrum_value in zip(es_drs, spectrum):
+                this_diff_rate += (e_dr[1] * spectrum_value)
+            diff_rates.append(this_diff_rate)
+
+        return diff_rates
+
     def get_diff_rate_source(self, source):
         this_source = deepcopy(source)
-
         this_source.set_data(self.base_source.data, data_is_annotated=True)
+
+        diff_rates = []
         for i_batch in range(this_source.n_batches):
             q = this_source.data_tensor[i_batch]
-            print(this_source.model_blocks[0]._compute(q, None, energy=None))
+            if i_batch == this_source.n_batches - 1:
+                energies_diff_rates = self.base_source.data['energies_diff_rates'][i_batch * self.base_source.batch_size::].values
+                spectrum_values = fd.tf_to_np(this_source.model_blocks[0]._compute(q, None, energy=None))
+
+            else:
+                energies_diff_rates = self.base_source.data['energies_diff_rates'][i_batch * self.base_source.batch_size:(i_batch + 1) * self.base_source.batch_size]
+                spectrum_values = fd.tf_to_np(this_source.model_blocks[0]._compute(q, None, energy=None))
+
+            diff_rates.extend(self.scale_by_spectrum(energies_diff_rates, spectrum_values))
+
+        return diff_rates
