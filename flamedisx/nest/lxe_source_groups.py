86c76adc02eddfa6b970aba6eebcaccf7c380b84
Robert James <robert.james.19@ucl.ac.uk>
2022-12-05 18:04:49 -0700

    Some bug fixes. Attempt at trying something...

--- flamedisx/nest/lxe_source_groups.py
+++ flamedisx/nest/lxe_source_groups.py
@@ -245,14 +245,14 @@ class nestERSourceGroup(BlockModelSourceGroup, fd_nest.nestERSource):
     model_blocks_read_in = (
         fd_nest.SGMakePhotonsElectronER,)
 
-    def __init__(self, caching=False, **kwargs):
-        if caching is True:
-            self.no_step_dimensions = self.no_step_dimensions + \
-                ('photons_produced',
-                 'electrons_produced',
-                 'energy')
-
-        super().__init__(**kwargs)
+    # def __init__(self, caching=False, **kwargs):
+    #     if caching is True:
+    #         self.no_step_dimensions = self.no_step_dimensions + \
+    #             ('photons_produced',
+    #              'electrons_produced',
+    #              'energy')
+    #
+    #     super().__init__(**kwargs)
 
 
 @export
