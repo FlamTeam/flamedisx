1b1d3ab4061ca364abbd1aed28079891e77bbb4d
Robert James <robert.james.19@ucl.ac.uk>
2023-01-05 14:18:40 +0000

    Add assertion that energies match between base source and real source with source groups.

--- flamedisx/nest/source_group.py
+++ flamedisx/nest/source_group.py
@@ -154,7 +154,8 @@ class SourceGroup:
     def get_diff_rate_source(self, source):
         this_source = deepcopy(source)
 
-        assert self.base_source.batch_size == this_source.batch_size
+        assert self.base_source.batch_size == this_source.batch_size, "source_group_type and source must have the same batch size"
+        assert (self.base_source.energies.all == source.energies).all(), "source_group_type and source must have the same energies in their spectra"
 
         this_source.set_data(self.base_source.data, data_is_annotated=True)
 
