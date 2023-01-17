213271eaf4f85976fcf2261f81d09eaeed491e7e
Robert James <robert.james.19@ucl.ac.uk>
2023-01-17 23:41:01 +0100

    Add version of non-asymptotic inference code for when using template sources (just the toy TS part done, need to do the observed TS part).

--- flamedisx/non_asymptotic_inference_templates.py
+++ flamedisx/non_asymptotic_inference_templates.py
@@ -4,6 +4,8 @@ from scipy import stats
 import pickle as pkl
 from tqdm.auto import tqdm
 import typing as ty
+from numpy.typing import NDArray
+from math import isclose
 
 import tensorflow as tf
 
@@ -11,7 +13,7 @@ export, __all__ = fd.exporter()
 
 
 @export
-class FrequentistIntervalRatesOnly():
+class FrequentistIntervalRatesOnlyTemplates():
     """NOTE: currently works for a single dataset only.
 
     Arguments:
@@ -19,24 +21,15 @@ class FrequentistIntervalRatesOnly():
             masses) of which we want to get limits/intervals
         - background_source_names: tuple of names for background sources
         - sources: dictionary {sourcename: class} of all signal and background source classes
-        - dictionary {sourcename: {kwarg1: value, ...}, ...}, for
+        - arguments: dictionary {sourcename: {kwarg1: value, ...}, ...}, for
             passing keyword arguments to source constructors
-        - pre_estimated_mus: dictionary {sourcename: mu}, to pass in pre-estiamted mus, for consistency
-        - max_rm_dict: dictionary {sourcename: max_rm}, where max_rm is the highest rate multiplier that
-            will be used for simulating a toy dataset for a given source, to control the reservoir size
-        - batch_size_diff_rate: batch size that will be used for the reservoir differential rate computation
-        - batch_size_rates: batch size that will be used for the RM fits (can be much larger!)
-        - max_sigma: to be passed to source classes used for the reservoir
-        - max_sigma_outer: to be passed to source classes used for the reservoir
+        - batch_size: batch size that will be used for the RM fits
         - rate_gaussian_constraints: dictionary {sourcename: (mu, sigma)} to pass the mean and width for
             any Gaussian constraints on background rate multipliers
+        - other_constraints:
         - rm_bounds: dictionary {sourcename: (lower, upper)} to set fit bounds on the rate multipliers
-        - defaults: dictionary of default parameter values to use for all sources
         - ntoys: number of toys that will be run to get test statistic distributions
-        - input_reservoir: path to input reservoir, if we wish to pass this
-        - skip_reservoir: option to skip reservoir population, if no input_reservoir passed but no
-            reservoir is required (e.g. for when calculating an observed test statistic)
-
+        - log_constraint_fn:
     """
 
     def __init__(
@@ -45,38 +38,22 @@ class FrequentistIntervalRatesOnly():
             background_source_names: ty.Tuple[str],
             sources: ty.Dict[str, fd.Source.__class__],
             arguments: ty.Dict[str, ty.Dict[str, ty.Union[int, float]]] = None,
-            pre_estimated_mus: ty.Dict[str, float] = None,
-            max_rm_dict: ty.Dict[str, float] = None,
-            batch_size_diff_rate=100,
-            batch_size_rates=10000,
-            max_sigma=None,
-            max_sigma_outer=None,
+            batch_size=10000,
             rate_gaussian_constraints: ty.Dict[str, ty.Tuple[float, float]] = None,
+            other_constraints: ty.Dict[str, ty.Tuple[NDArray[float], NDArray[float]]] = None,
             rm_bounds: ty.Dict[str, ty.Tuple[float, float]] = None,
-            defaults=None,
             ntoys=1000,
-            input_reservoir=None,
-            skip_reservoir=False):
-
-        if arguments is None:
-            arguments = dict()
-
-        if pre_estimated_mus is None:
-            self.pre_estimated_mus = dict()
-            for key in sources.keys():
-                self.pre_estimated_mus[key] = None
-        else:
-            self.pre_estimated_mus = pre_estimated_mus
+            log_constraint_fn=None):
 
         for key in sources.keys():
             if key not in arguments.keys():
                 arguments[key] = dict()
 
-        if defaults is None:
-            defaults = dict()
-
         if rate_gaussian_constraints is None:
             rate_gaussian_constraints = dict()
+
+        if other_constraints is None:
+            other_constraints = dict()
 
         if rm_bounds is None:
             rm_bounds = dict()
@@ -84,41 +61,31 @@ class FrequentistIntervalRatesOnly():
             for bounds in rm_bounds.values():
                 assert bounds[0] >= 0., 'Currently do not support negative rate multipliers'
 
+        if log_constraint_fn is None:
+            def log_constraint_fn(**kwargs):
+                return 0.
+            self.log_constraint_fn = log_constraint_fn
+        else:
+            self.log_constraint_fn = log_constraint_fn
+
         self.signal_source_names = signal_source_names
         self.background_source_names = background_source_names
 
         self.ntoys = ntoys
-        self.batch_size_diff_rate = batch_size_diff_rate
-        self.batch_size_rates = batch_size_rates
+        self.batch_size = batch_size
 
         self.rate_gaussian_constraints = {f'{key}_rate_multiplier': value for
                                           key, value in rate_gaussian_constraints.items()}
+        self.other_constraints = {f'{key}_rate_multiplier': value for
+                                  key, value in other_constraints.items()}
         self.rm_bounds = rm_bounds
 
         self.test_stat_dists = dict()
         self.observed_test_stats = dict()
         self.p_vals = dict()
 
-        # Create source instances
         self.sources = sources
-        self.source_objects = {
-            sname: sclass(**(arguments.get(sname)),
-                          data=None,
-                          max_sigma=max_sigma,
-                          max_sigma_outer=max_sigma_outer,
-                          batch_size=self.batch_size_diff_rate,
-                          **defaults)
-            for sname, sclass in sources.items()}
-
-        if not skip_reservoir:
-            if input_reservoir is not None:
-                # Read in frozen source reservoir
-                self.reservoir = pkl.load(open(input_reservoir, 'rb'))
-            else:
-                # Create frozen source reservoir
-                self.reservoir = fd.frozen_reservoir.make_event_reservoir(ntoys=ntoys,
-                                                                          max_rm_dict=max_rm_dict,
-                                                                          **self.source_objects)
+        self.arguments = arguments
 
     def test_statistic_tmu_tilde(self, mu_test, signal_source_name, likelihood, guess_dict):
         """Internal function to evaluate the test statistic of equation 11 in
@@ -157,34 +124,32 @@ class FrequentistIntervalRatesOnly():
             return
 
         assert mus_test is not None, 'Must pass in mus to be scanned over'
-        assert self.reservoir is not None, 'Must populate frozen source reservoir'
 
         self.test_stat_dists = dict()
         unconditional_best_fits = dict()
-        test_stats_no_signal = dict()
-        unconditional_best_fits_no_signal = dict()
+        # test_stats_no_signal = dict()
+        # unconditional_best_fits_no_signal = dict()
         # Loop over signal sources
         for signal_source in self.signal_source_names:
             test_stat_dists = dict()
             unconditional_bfs = dict()
-            ts_no_signal = dict()
-            unconditional_bfs_no_signal = dict()
+            # ts_no_signal = dict()
+            # unconditional_bfs_no_signal = dict()
 
             sources = dict()
+            arguments = dict()
             for background_source in self.background_source_names:
                 sources[background_source] = self.sources[background_source]
+                arguments[background_source] = self.arguments[background_source]
             sources[signal_source] = self.sources[signal_source]
+            arguments[signal_source] = self.arguments[signal_source]
 
-            # Create likelihood of FrozenReservoirSources
-            likelihood_fast = fd.LogLikelihood(sources={sname: fd.FrozenReservoirSource for sname in sources.keys()},
-                                               arguments={sname: {'source_type': sclass,
-                                                                  'source_name': sname,
-                                                                  'reservoir': self.reservoir,
-                                                                  'input_mu': self.pre_estimated_mus[sname]}
-                                                          for sname, sclass in sources.items()},
-                                               progress=False,
-                                               batch_size=self.batch_size_rates,
-                                               free_rates=tuple([sname for sname in sources.keys()]))
+            # Create likelihood of TemplateSources
+            likelihood = fd.LogLikelihood(sources=sources,
+                                          arguments=arguments,
+                                          progress=False,
+                                          batch_size=self.batch_size,
+                                          free_rates=tuple([sname for sname in sources.keys()]))
 
             rm_bounds = dict()
             if signal_source in self.rm_bounds.keys():
@@ -198,41 +163,35 @@ class FrequentistIntervalRatesOnly():
                     rm_bounds[background_source] = (0., None)
 
             # Pass rate multiplier bounds to likelihood
-            likelihood_fast.set_rate_multiplier_bounds(**rm_bounds)
+            likelihood.set_rate_multiplier_bounds(**rm_bounds)
 
-            # Set up Gaussian log constraint for background rate multipliers: we center the
-            # constraint on the value of the multipliers we simualate at for that toy
-            def log_constraint(**kwargs):
-                log_constraint = sum(-0.5 * ((value - kwargs[f'{key}_constraint'][0]) /
-                                     kwargs[f'{key}_constraint'][1])**2 for key, value in kwargs.items()
-                                     if key in self.rate_gaussian_constraints.keys())
-                return log_constraint
-            likelihood_fast.set_log_constraint(log_constraint)
+            # Pass constraint function to likelihood
+            likelihood.set_log_constraint(self.log_constraint_fn)
 
             # Save the test statistic values and unconditional best fits for each toy, for each
             # signal RM scanned over, for this signal source
             these_mus_test = mus_test[signal_source]
             # Loop over signal rate multipliers
             for mu_test in tqdm(these_mus_test, desc='Scanning over mus'):
-                ts_dist = self.toy_test_statistic_dist(mu_test, signal_source, likelihood_fast)
+                ts_dist = self.toy_test_statistic_dist(mu_test, signal_source, likelihood)
                 test_stat_dists[mu_test] = ts_dist[0]
                 unconditional_bfs[mu_test] = ts_dist[1]
-                ts_no_signal[mu_test] = ts_dist[2]
-                unconditional_bfs_no_signal[mu_test] = ts_dist[3]
+                # ts_no_signal[mu_test] = ts_dist[2]
+                # unconditional_bfs_no_signal[mu_test] = ts_dist[3]
 
             self.test_stat_dists[signal_source] = test_stat_dists
             unconditional_best_fits[signal_source] = unconditional_bfs
-            test_stats_no_signal[signal_source] = ts_no_signal
-            unconditional_best_fits_no_signal[signal_source] = unconditional_bfs_no_signal
+            # test_stats_no_signal[signal_source] = ts_no_signal
+            # unconditional_best_fits_no_signal[signal_source] = unconditional_bfs_no_signal
 
         # Output test statistic distributions and fits
         if dists_output_name is not None:
             pkl.dump(self.test_stat_dists, open(dists_output_name, 'wb'))
             pkl.dump(unconditional_best_fits, open(dists_output_name[:-4] + '_fits.pkl', 'wb'))
-            pkl.dump(test_stats_no_signal, open(dists_output_name[:-4] + '_no_signal.pkl', 'wb'))
-            pkl.dump(test_stats_no_signal, open(dists_output_name[:-4] + '_fits_no_signal.pkl', 'wb'))
+            # pkl.dump(test_stats_no_signal, open(dists_output_name[:-4] + '_no_signal.pkl', 'wb'))
+            # pkl.dump(test_stats_no_signal, open(dists_output_name[:-4] + '_fits_no_signal.pkl', 'wb'))
 
-    def toy_test_statistic_dist(self, mu_test, signal_source_name, likelihood_fast):
+    def toy_test_statistic_dist(self, mu_test, signal_source_name, likelihood):
         """Internal function to get a test statistic distribution for a given signal source
         and signal RM.
         """
@@ -240,47 +199,67 @@ class FrequentistIntervalRatesOnly():
 
         ts_values = []
         unconditional_bfs = []
-        ts_values_no_signal = []
-        unconditional_bfs_no_signal = []
+        # ts_values_no_signal = []
+        # unconditional_bfs_no_signal = []
 
         # Loop over toys
         for toy in tqdm(range(self.ntoys), desc='Doing toys'):
             constraint_extra_args = dict()
             for background_source in self.background_source_names:
                 # Prepare to sample background RMs from constraint functions
-                assert background_source in self.rm_bounds.keys(), \
-                    'Must provide bounds when using a Gaussian constraint'
-                domain = np.linspace(self.rm_bounds[background_source][0], self.rm_bounds[background_source][1], 1000)
-                gaussian_constraint = self.rate_gaussian_constraints[f'{background_source}_rate_multiplier']
-                constraint = np.exp(-0.5 * ((domain - gaussian_constraint[0]) / gaussian_constraint[1])**2)
-                constraint /= np.sum(constraint)
+                if f'{background_source}_rate_multiplier' in self.rate_gaussian_constraints:
+                    assert background_source in self.rm_bounds.keys(), \
+                        'Must provide bounds when using a Gaussian constraint'
+                    domain = np.linspace(self.rm_bounds[background_source][0], self.rm_bounds[background_source][1], 1000)
+                    gaussian_constraint = self.rate_gaussian_constraints[f'{background_source}_rate_multiplier']
+                    constraint = np.exp(-0.5 * ((domain - gaussian_constraint[0]) / gaussian_constraint[1])**2)
+                    constraint /= np.sum(constraint)
+
+                elif f'{background_source}_rate_multiplier' in self.other_constraints:
+                    domain = self.other_constraints[f'{background_source}_rate_multiplier'][0]
+                    constraint = self.other_constraints[f'{background_source}_rate_multiplier'][1]
+                    assert np.shape(domain) == np.shape(constraint), \
+                        'Shapes of contraint domain and values do not match'
+                    assert isclose(np.sum(constraint), 1.), \
+                        'Constraint values should sum to 1'
+
+                else:
+                    assert background_source in self.rm_bounds.keys(), \
+                        'Must provide bounds if no constraint provided'
+                    domain = np.linspace(self.rm_bounds[background_source][0], self.rm_bounds[background_source][1], 1000)
+                    constraint = np.ones_like(domain)
+                    constraint /= np.sum(constraint)
 
                 # Sample background RMs from constraint functions
                 draw = tf.cast(np.random.choice(domain, 1, p=constraint)[0], fd.float_type())
                 rm_value_dict[f'{background_source}_rate_multiplier'] = draw
                 # Recall: we want to shift the constraint in the likelihood based on the background RMs we draw
-                constraint_extra_args[f'{background_source}_rate_multiplier_constraint'] = \
-                    (draw, tf.cast(self.rate_gaussian_constraints[f'{background_source}_rate_multiplier'][1],
-                                   fd.float_type()))
+                if f'{background_source}_rate_multiplier' in self.rate_gaussian_constraints:
+                    constraint_extra_args[f'{background_source}_rate_multiplier_constraint'] = \
+                        (draw, tf.cast(self.rate_gaussian_constraints[f'{background_source}_rate_multiplier'][1],
+                                       fd.float_type()))
+                elif f'{background_source}_rate_multiplier' in self.other_constraints:
+                    constraint_extra_args[f'{background_source}_rate_multiplier_constraint'] = \
+                        (draw, 0.)
 
             # Shift the constraint in the likelihood based on the background RMs we drew
-            likelihood_fast.set_constraint_extra_args(**constraint_extra_args)
+            likelihood.set_constraint_extra_args(**constraint_extra_args)
 
             # Simulate and set data
-            toy_data = likelihood_fast.simulate(**rm_value_dict)
+            toy_data = likelihood.simulate(**rm_value_dict)
 
-            likelihood_fast.set_data(toy_data)
+            likelihood.set_data(toy_data)
 
-            ts_result = self.test_statistic_tmu_tilde(mu_test, signal_source_name, likelihood_fast, rm_value_dict)
+            ts_result = self.test_statistic_tmu_tilde(mu_test, signal_source_name, likelihood, rm_value_dict)
             ts_values.append(ts_result[0])
             unconditional_bfs.append(ts_result[1])
 
-            if len(toy_data[toy_data['source'] == signal_source_name]) == 0:
-                ts_values_no_signal.append(ts_result[0])
-                unconditional_bfs_no_signal.append(ts_result[1])
+            # if len(toy_data[toy_data['source'] == signal_source_name]) == 0:
+            #     ts_values_no_signal.append(ts_result[0])
+            #     unconditional_bfs_no_signal.append(ts_result[1])
 
         # Return the test statistic and unconditional best fit
-        return ts_values, unconditional_bfs, ts_values_no_signal, unconditional_bfs_no_signal
+        return ts_values, unconditional_bfs #, ts_values_no_signal, unconditional_bfs_no_signal
 
     def get_observed_test_stats(self, mus_test=None, data=None, input_test_stats=None, test_stats_output_name=None,
                                 mu_estimators=None):
