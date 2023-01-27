e7c5bbb914e05c216140b40cb7552770a493231d
Robert James <robert.james.19@ucl.ac.uk>
2023-01-27 13:45:26 +0100

    Update neutrino spectrum to prepare for using source groups and caching.

--- flamedisx/nest/background_sources.py
+++ flamedisx/nest/background_sources.py
@@ -22,7 +22,7 @@ export, __all__ = fd.exporter()
 class vERSource(fd_nest.nestERSource):
     """ER background source from solar neutrinos (PP+7Be+CNO).
     Reads in energy spectrum from .pkl file, generated with LZ's DMCalc.
-    Normalise such that the spectrum predicts 115.00 events in 1 tonne year.
+    Normalise such that the spectrum predicts 30.76 events in 1 tonne year.
     """
 
     def __init__(self, *args, fid_mass=1., livetime=1., **kwargs):
@@ -32,7 +32,7 @@ class vERSource(fd_nest.nestERSource):
         df_vER = pd.read_pickle(os.path.join(os.path.dirname(__file__), 'background_spectra/vER_spectrum.pkl'))
 
         self.energies = tf.convert_to_tensor(df_vER['energy_keV'].values, dtype=fd.float_type())
-        scale = fid_mass * livetime * 215.
+        scale = fid_mass * livetime * 30.76
         self.rates_vs_energy = tf.convert_to_tensor(df_vER['spectrum_value_norm'].values * scale, dtype=fd.float_type())
 
         super().__init__(*args, **kwargs)
