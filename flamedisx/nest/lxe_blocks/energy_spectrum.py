30868289c5acd2e1d2b5788f6c8f52b7b4d179cb
Robert James <robert.james.19@ucl.ac.uk>
2023-03-29 02:58:47 +0200

    Try something else.

--- flamedisx/nest/lxe_blocks/energy_spectrum.py
+++ flamedisx/nest/lxe_blocks/energy_spectrum.py
@@ -248,7 +248,7 @@ class FixedShapeEnergySpectrumNR(FixedShapeEnergySpectrum):
 
 @export
 class FixedShapeEnergySpectrumER(FixedShapeEnergySpectrum):
-    max_dim_size = {'energy': 50}
+    max_dim_size = {'energy': 100}
 
 
 @export
@@ -317,7 +317,7 @@ class SpatialRateEnergySpectrumNR(SpatialRateEnergySpectrum):
 
 @export
 class SpatialRateEnergySpectrumER(SpatialRateEnergySpectrum):
-    max_dim_size = {'energy': 50}
+    max_dim_size = {'energy': 100}
 
 
 ##
