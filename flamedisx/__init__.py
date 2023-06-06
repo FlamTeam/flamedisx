6b76ad5b4786cd49c4eb0291ac74d4e6cf434cb0
Robert James <robert.james.19@ucl.ac.uk>
2023-06-06 19:52:30 +0300

    Deprecate old file.

--- flamedisx/__init__.py
+++ flamedisx/__init__.py
@@ -10,7 +10,6 @@ from .bounds import *
 from .mu_estimation import *
 from .frozen_reservoir import *
 from .non_asymptotic_inference import *
-from .non_asymptotic_inference_templates import *
 
 # Original flamedisx models
 # Accessible under fd root package (for now), for backwards compatibility
