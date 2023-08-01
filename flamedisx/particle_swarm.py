78c1d77858df623304d313f01eea59ed2646eadb
Robert James <robert.james.19@ucl.ac.uk>
2023-08-01 23:24:42 +0100

    Remove PSO stuff, most of structure for iterative fit procedure.

--- flamedisx/particle_swarm.py
+++ flamedisx/particle_swarm.py
@@ -14,151 +14,72 @@ class PSOColumnSource(fd.ColumnSource):
         super().__init__(*args, **kwargs)
 
 
-class Particles():
-    """
-    """
-    def __init__(self,
-                 sources: ty.Dict[str, fd.Source] = None,
-                 fit_params: ty.Tuple[str] = None,
-                 bounds: ty.Dict[str, ty.Tuple[float]] = None,
-                 guess_dict: ty.Dict[str, float] = None,
-                 log_constraint = None,
-                 n_particles=100):
-
-        self.sources = sources
-
-        self.fit_params = fit_params
-        self.bounds = bounds
-        self.guess_dict = guess_dict
-
-        if log_constraint is None:
-            def log_constraint(**kwargs):
-                return 0.
-        self.log_constraint = log_constraint
-
-        self.n_particles = n_particles
-
-        self.X = np.zeros((len(fit_params), n_particles))
-        self.V = np.zeros((len(fit_params), n_particles))
-
-        self.initialise_particles()
-
-    def initialise_particles(self):
-
-        for i, param in enumerate(self.fit_params):
-            self.X[i, :] = np.random.uniform(size=self.n_particles) * \
-                (self.bounds[param][1] - self.bounds[param][0]) + self.bounds[param][0]
-            self.V[i, :] = np.zeros_like(self.X[i, :])
-
-        self.pbest = self.X
-        self.pbest_obj = np.zeros(len(self.X[0, :]))
-
-        for i in range(len(self.pbest_obj)):
-            eval_dict = dict()
-            for j, param in enumerate(self.fit_params):
-                eval_dict[param] = self.X[j, i]
-
-            self.pbest_obj[i] = -2. * self.likelihood(**eval_dict)
-
-        self.gbest = self.pbest[:, self.pbest_obj.argmin()]
-        self.gbest_obj = self.pbest_obj.min()
-
-    def update_particles(self, c1, c2, w):
-
-        r1, r2 = np.random.rand(2)
-        self.V = w * self.V + c1 * r1 * (self.pbest - self.X) + \
-            c2 * r2 * (self.gbest.reshape(-1, 1) - self.X)
-        self.X = self.X + self.V
-
-        for i, param in enumerate(self.fit_params):
-            if param[-16:] == '_rate_multiplier':
-                self.X[i, :][self.X[i, :] < 1e-9] = 1e-9
-
-        obj = np.zeros_like(self.pbest_obj)
-        for i in range(len(obj)):
-            eval_dict = dict()
-            for j, param in enumerate(self.fit_params):
-                eval_dict[param] = self.X[j, i]
-
-            obj[i] = -2. * self.likelihood(**eval_dict)
-
-        self.pbest[:, (self.pbest_obj >= obj)] = self.X[:, (self.pbest_obj >= obj)]
-        self.pbest_obj = np.array([self.pbest_obj, obj]).min(axis=0)
-
-        self.gbest = self.pbest[:, self.pbest_obj.argmin()]
-        self.gbest_obj = self.pbest_obj.min()
-
-    def likelihood(self, **params):
-        params_filter = dict()
-        for key, value in params.items():
-            if key[-16:] == '_rate_multiplier':
-                continue
-            params_filter[key] = value
-
-        dr_sum = 0.
-        mu_sum = 0.
-        for sname, source in self.sources.items():
-            dr_sum += source.batched_differential_rate(**params_filter, progress=False) * params[f'{sname}_rate_multiplier']
-            mu_sum += source.estimate_mu(**params_filter, fast=True) * params[f'{sname}_rate_multiplier']
-
-        return (-mu_sum + np.sum(np.log(dr_sum)) + self.log_constraint(**params))
-
-
 @export
 class PSOOptimiser():
     """
     """
     def __init__(self,
-                 regular_sources: ty.Dict[str, fd.Source] = None,
-                 column_sources: ty.Dict[str, fd.Source] = None,
+                 regular_sources: ty.Dict[str, fd.Source.__class__] = None,
+                 column_sources: ty.Dict[str, fd.Source.__class__] = None,
                  data: pd.DataFrame = None,
                  fit_params: ty.Tuple[str] = None,
                  bounds: ty.Dict[str, ty.Tuple[float]] = None,
                  guess_dict: ty.Dict[str, float] = None,
                  log_constraint = None,
-                 n_particles=100,
-                 n_iterations=200,
-                 c1=1., c2=1., w=0.8):
+                 batch_size=5000,
+                 n_iterations=5):
 
+        self.guess_dict = guess_dict
         shape_params = dict()
-        for key, value in guess_dict.items():
+        for key, value in self.guess_dict.items():
             if key[-16:] == '_rate_multiplier':
                 continue
             shape_params[key] = value
 
-        for sname, source in column_sources.items():
-            source.set_data(data)
-            data[f'{sname}_diff_rate'] = source.batched_differential_rate(**shape_params, progress=False)
+        self.column_sources = column_sources
+        for sname, source in self.column_sources.items():
+            s = source(batch_size=batch_size)
+            s.set_data(data)
+            data[f'{sname}_diff_rate'] = s.batched_differential_rate(**shape_params, progress=False)
+
+        self.data = data
+
+        column_names = dict()
+        self.mus = dict()
+        for sname, source in self.column_sources.items():
+            column_names[sname] = f'{sname}_diff_rate'
+            self.mus[sname] = source().estimate_mu(**shape_params)
+
+        est = dict()
+        for sname, source in regular_sources.items():
+            est[sname] = fd.SimulateEachCallMu(source=source())
+        for sname, source in self.column_sources.items():
+            est[sname] = fd.ConstantMu(source=PSOColumnSource(mu=self.mus[sname]))
 
         sources = dict()
+        arguments = dict()
         for sname, source in regular_sources.items():
             sources[sname] = source
-        for sname, source in column_sources.items():
-            mu = source.estimate_mu(**shape_params)
-            sources[sname] = PSOColumnSource(column_name=f'{sname}_diff_rate', mu=mu,
-                                             batch_size=len(data))
+            arguments[sname] = dict()
+        for sname in self.column_sources.keys():
+            sources[sname] = PSOColumnSource
+            arguments[sname] = {'column_name': column_names[sname]}
 
-        for source in sources.values():
-            source.set_data(data)
+        self.likelihood = fd.LogLikelihood(sources=sources,
+                                           arguments=arguments,
+                                           free_rates={sname for sname in sources.keys()},
+                                           batch_size=batch_size,
+                                           log_constraint=log_constraint,
+                                           mu_estimators = est,
+                                           **bounds
+                                          )
 
         self.n_iterations = n_iterations
 
-        self.particles = Particles(sources=sources, fit_params=fit_params,
-                                   bounds=bounds, guess_dict=guess_dict,
-                                   log_constraint=log_constraint,
-                                   n_particles=n_particles)
-
-        self.c1 = c1
-        self.c2 = c2
-        self.w = w
-
-    def run_routine(self):
-        for i in range(self.n_iterations):
-            self.particles.update_particles(self.c1, self.c2, self.w)
-
     def bestfit(self):
-        bf = dict()
-        for i, param in enumerate(self.particles.fit_params):
-            bf[param] = self.particles.gbest[i]
+        self.likelihood.set_data(self.data)
 
-        return bf
+        bf = self.likelihood.bestfit(guess=self.guess_dict)
+        print(bf)
+
+        # for i in range(self.n_iterations):
