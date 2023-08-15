8ff4dbdb5ff2516e91e94cfdb0b95aa821e7bf7d
Robert James <robert.james.19@ucl.ac.uk>
2023-08-16 00:58:54 +0100

    Don't randomise constraint centers.

--- flamedisx/non_asymptotic_inference.py
+++ flamedisx/non_asymptotic_inference.py
@@ -375,8 +375,7 @@ class TSEvaluation():
 
             # Sample constraint centers
             if background_source in self.gaussian_constraint_widths:
-                draw = stats.norm.rvs(loc=expected_background_counts,
-                                      scale=self.gaussian_constraint_widths[background_source])
+                draw = expected_background_counts
                 constraint_extra_args[f'{background_source}_expected_counts'] = tf.cast(draw, fd.float_type())
 
             elif background_source in self.sample_other_constraints:
