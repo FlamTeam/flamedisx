8d45e43aca15ad2f652220ea01572342ca57c1b1
Cecilia Ferrari <cecilia.ferrari@gssi.it>
2024-01-09 11:39:37 +0100

    re-fixing int bug

--- flamedisx/lxe_blocks/raw_signals.py
+++ flamedisx/lxe_blocks/raw_signals.py
@@ -71,7 +71,7 @@ class MakeFinalSignals(fd.Block):
         # add offset to std to avoid NaNs from norm.pdf if std = 0
         if self.quanta_name == 'electron':
             result = tfp.distributions.Poisson(
-                rate = mean).prob(tf.cast(s_observed, tf.int32))
+                rate = mean).prob(s_observed)
         else:
             result = tfp.distributions.Normal(
                 loc=mean, scale=std + 1e-10
