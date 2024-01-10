5d8c14bfdc36f0ae7fdd46b8ee1b9a3bf3207891
Cecilia Ferrari <cecilia.ferrari@gssi.it>
2024-01-10 16:48:42 +0100

    binomial float fixing bug

--- flamedisx/lxe_blocks/s2_loss.py
+++ flamedisx/lxe_blocks/s2_loss.py
@@ -16,7 +16,7 @@ class MakeS2AfterLoss(fd.Block):
 
     max_dim_size = {'s2_raw': 240}
 
-    s2_survival_p = 1
+    s2_survival_p = 1.
 
     def _compute(self, data_tensor, ptensor,
                  s2_raw, s2_raw_after_loss):
@@ -24,7 +24,7 @@ class MakeS2AfterLoss(fd.Block):
                            data_tensor=data_tensor, ptensor=ptensor)[:, o, o]
 
         # s2_raw_after_loss distributed as Binom(s2_raw, p=s2_survival_probability)
-        s2_raw_after_loss = tf.clip_by_value(s2_raw_after_loss, 1e-15, tf.float32.max)
+        s2_raw_after_loss = tf.clip_by_value(s2_raw_after_loss, 0, tf.int32.max)
         result = tfp.distributions.Binomial(
                 total_count=tf.clip_by_value(tf.cast(s2_raw, dtype=fd.int_type()),1e-15, tf.int32.max),
                 probs=tf.clip_by_value(tf.cast(s2_survival_probability, dtype=fd.float_type()), 0.,1.)
