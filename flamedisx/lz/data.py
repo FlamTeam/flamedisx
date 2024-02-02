f7e651fd78b3f5c9f6d7f6c284ee391505eb47e3
Robert James <robert.james.19@ucl.ac.uk>
2024-02-02 16:29:12 +1100

    Update access method for LZ private data.

--- flamedisx/lz/data.py
+++ flamedisx/lz/data.py
@@ -5,6 +5,7 @@ import os
 import os.path
 import random
 import string
+import sys
 
 import flamedisx as fd
 export, __all__ = fd.exporter()
@@ -32,26 +33,46 @@ def ensure_token(token=None):
     return user, token
 
 
-def ensure_repo(repo_name, repo_path, user=None, token=None):
-    """Clones private repository (prompting for credentials) if we do not have it"""
-    if not os.path.exists(repo_path):
-        print("Private data requested, we must clone repository folder.")
-        user, token = ensure_token()
-        temp_folder = ''.join(random.choices(string.ascii_lowercase, k=8))
-        fd.run_command(f'git clone https://{user}:{token}'
-                       f'@gitlab.com/{repo_name} {temp_folder}')
-        fd.run_command(f'mv {temp_folder}/{repo_path} .')
-        fd.run_command(f'rm -r -f {temp_folder}')
+def clone_repo(repo_name, repo_path, user=None, token=None):
+    """Clones private repository (prompting for credentials)"""
+    fd.run_command(f'rm -r -f {repo_path}')
+    user, token = ensure_token()
+    temp_folder = ''.join(random.choices(string.ascii_lowercase, k=8))
+    fd.run_command(f'git clone https://{user}:{token}'
+                    f'@gitlab.com/{repo_name} {temp_folder}')
+    fd.run_command(f'mv {temp_folder}/{repo_path} .')
+    fd.run_command(f'rm -r -f {temp_folder}')
 
 
 @export
-def clone_lz_repo():
-    ensure_repo('luxzeplin/stats/LZFlameFit.git', PATH)
-
-
-@export
-def get_lz_file(data_file_name):
-    """Return information from file in lz_private_data/...
+def lz_setup(mode='tensors', run='sr1', lz_private_data_dir=None):
+    """Update the system path to include necessary LZ private data files
     """
-    ensure_repo('luxzeplin/stats/LZFlameFit.git', PATH)
-    return fd.get_resource(f'{PATH}/{data_file_name}')
+    if lz_private_data_dir is not None:
+        if os.path.isdir(f'{lz_private_data_dir}/{mode}/{run}'):
+            sys.path.append(f'{lz_private_data_dir}/{mode}/{run}')
+            print(f'Successfully found mode {mode} and run {run} in ' + \
+                  f'directory {lz_private_data_dir}')
+            return
+        else:
+            print(f'Error finding mode {mode} and run {run} in directory {lz_private_data_dir}')
+
+    print('Seeing if LZ private data directory already exists')
+
+    if os.path.isdir(f'lz_private_data/{mode}/{run}'):
+        sys.path.append(f'lz_private_data/{mode}/{run}')
+        print(f'Successfully found mode {mode} and run {run} in directory lz_private_data')
+        return
+    else:
+        print(f'Mode {mode} and run {run} not found in any existing directory')
+
+    print('Cloning repo, requires LZ GitLab access token')
+
+    clone_repo('luxzeplin/stats/LZFlameFit.git', PATH)
+
+    if os.path.isdir(f'lz_private_data/{mode}/{run}'):
+        sys.path.append(f'lz_private_data/{mode}/{run}')
+        print(f'Successfully found mode {mode} and run {run} in cloned directory')
+    else:
+        raise RuntimeError(f'Either the cloning failed, or could not find ' + \
+                           f'mode {mode} and run {run} in cloned directory')
