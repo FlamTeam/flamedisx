no longer complains of tensor wrong shape while fitting but llh is infinite

flamedisx/lxe_blocks/reconstruct_signals.py
@@ -43,8 +43,8 @@ class ReconstructSignals(fd.Block):
         d['p_accepted'] *= self.gimme_numpy(self.signal_name + '_acceptance')
 
     def _annotate(self, d):
-        bias = self.gimme_numpy('reconstruction_bias_compute_' + self.signal_name)
-        smear = self.gimme_numpy('reconstruction_smear_compute_' + self.signal_name)
+        bias = self.gimme_numpy('reconstruction_bias_annotate_' + self.signal_name)
+        smear = self.gimme_numpy('reconstruction_smear_annotate_' + self.signal_name)
         mle = d[self.raw_signal_name + '_mle'] = \
             (d[self.signal_name] / bias).clip(0, None)
 
@@ -68,30 +68,21 @@ class ReconstructSignals(fd.Block):
         bias = self.gimme('reconstruction_bias_simulate_' + self.signal_name,
                           data_tensor=data_tensor,
                           bonus_arg=s_raw,
-                          ptensor=ptensor)[:, o, o]
-
-        mu2 = s_observed/bias # incorrect but runs
-
-        # conceptually correct but somehow gives zero prob because s_raw is
-        # stuck at integers
+                          ptensor=ptensor)
         mu = s_raw * bias
 
         # add offset to std to avoid NaNs from norm.pdf if std = 0
         smear = self.gimme('reconstruction_smear_simulate_' + self.signal_name,
                            data_tensor=data_tensor,
                            bonus_arg=s_raw,
-                           ptensor=ptensor)[:, o, o] + 1e-15
+                           ptensor=ptensor) + 1e-15
 
         result = tfp.distributions.Normal(
             loc=mu, scale=smear).prob(s_observed)
 
-        tf.print('prob before eff: ', result)
-
         # Add detection/selection efficiency
         result *= self.gimme(self.signal_name + '_acceptance',
                              data_tensor=data_tensor, ptensor=ptensor)[:, o, o]
-        tf.print('prob after eff: ', result)
-        tf.print('*****')
         return result
 
     def check_data(self):
@@ -116,8 +107,8 @@ class ReconstructS1(ReconstructSignals):
         'reconstruction_smear_simulate_s1',)
     model_functions = (
         's1_acceptance',
-        'reconstruction_bias_compute_s1',
-        'reconstruction_smear_compute_s1',
+        'reconstruction_bias_annotate_s1',
+        'reconstruction_smear_annotate_s1',
         ) + special_model_functions
 
     max_dim_size = {'s1_raw': 120}
@@ -145,13 +136,13 @@ class ReconstructS1(ReconstructSignals):
         return tf.zeros_like(s1_raw, dtype=fd.float_type())
 
     # Getting from s1 -> s1_raw
-    def reconstruction_bias_compute_s1(self, s1):
+    def reconstruction_bias_annotate_s1(self, s1):
         """ Dummy method for pax s2 reconstruction bias mean. Overwrite
         it in source specific class. See x1t_sr1.py for example.
         """
         return tf.ones_like(s1, dtype=fd.float_type())
 
-    def reconstruction_smear_compute_s1(self, s1):
+    def reconstruction_smear_annotate_s1(self, s1):
         """ Dummy method for pax s2 reconstruction bias spread. Overwrite
         it in source specific class. See x1t_sr1.py for example.
         
@@ -182,8 +173,8 @@ class ReconstructS2(ReconstructSignals):
  )
     model_functions = (
         ('s2_acceptance',
-        'reconstruction_bias_compute_s2',
-        'reconstruction_smear_compute_s2',
+        'reconstruction_bias_annotate_s2',
+        'reconstruction_smear_annotate_s2',
         )
         + special_model_functions)
 
@@ -212,13 +203,13 @@ class ReconstructS2(ReconstructSignals):
         return tf.zeros_like(s2_raw, dtype=fd.float_type())
 
     # Getting from s2 -> s2_raw
-    def reconstruction_bias_compute_s2(self, s2):
+    def reconstruction_bias_annotate_s2(self, s2):
         """ Dummy method for pax s2 reconstruction bias mean. Overwrite
         it in source specific class. See x1t_sr1.py for example.
         """
         return tf.ones_like(s2, dtype=fd.float_type())
 
-    def reconstruction_smear_compute_s2(self, s2):
+    def reconstruction_smear_annotate_s2(self, s2):
         """ Dummy method for pax s2 reconstruction bias spread. Overwrite
         it in source specific class. See x1t_sr1.py for example.
 