b1807159b47fefbe69cfc28d6dd9412bf901b614
Robert James <robert.james.19@ucl.ac.uk>
2024-11-12 15:03:27 +1100

    Payne light yield model now implemented.

--- flamedisx/sabre/sabre_models.py
+++ flamedisx/sabre/sabre_models.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+import tensorflow_probability as tfp
+
 import pickle as pkl
 
 import flamedisx as fd
@@ -22,12 +24,65 @@ class SABRESource(fd.BlockModelSource):
 
         super().__init__(*args, **kwargs)
 
-    @staticmethod
-    def light_yield(energy, *, abs_ly=45.):
+    def light_yield(self, energy, *, abs_ly=45., eta_eh=0.534, dEdx_birks=166.):
         """
         """
-        ly = abs_ly
-        return ly
+        ly_relative_energies_keV = tf.experimental.numpy.geomspace(1., 450., num=100, dtype=fd.float_type())
+        ly_relative = self.light_yield_relative_interp(ly_relative_energies_keV, eta_eh, dEdx_birks)
+
+        ly_relative_interp = tfp.math.interp_regular_1d_grid(energy, 1., 450., ly_relative)
+
+        return abs_ly * ly_relative_interp
+
+    def light_yield_relative_interp(self, ly_relative_energies_keV, eta_eh, dEdx_birks):
+        """
+        """
+        def light_yield_relative(args):
+            #: Fixed model parameters: materials
+            Z_NaI = 64
+            M_NaI = 149.89
+            rho_NaI = 3.67 # g / cm^3
+            I_keV_NaI = 0.452 # keV
+
+            # Fixed parameters: model
+            c = 2.8
+            dEdx_ons = 36.4 # MeV / cm
+
+            def eta_cap(E_keV, eta_eh, dEdx_birks):
+                dEdx = dEdx_MeV_cm(E_keV)
+
+                numerator = 1. - eta_eh * tf.math.exp(-dEdx / dEdx_ons)
+                denominator = 1. + (dEdx / dEdx_birks)
+
+                return numerator / denominator
+
+            def dEdx_MeV_cm(E_keV):
+                E_ev = 1000. * E_keV
+                I_ev = 1000. * I_keV_NaI
+
+                prefactor = 785. * Z_NaI * rho_NaI / M_NaI
+                ln_arg = 1.16 * (E_ev + c * I_ev) / I_ev
+
+                dEdx_ev_angstrom = prefactor * tf.math.log(ln_arg) / E_ev
+                dEdx_MeV_cm = 1e-6 * 1e8 * dEdx_ev_angstrom
+
+                return dEdx_MeV_cm
+
+            E_keV = args[0]
+            eta_eh = args[1]
+            dEdx_birks = args[2]
+
+            x = tf.linspace(I_keV_NaI, E_keV, num=1000)
+            y = eta_cap(x, eta_eh, dEdx_birks)
+
+            return tfp.math.trapz(y, x=x) / (E_keV - I_keV_NaI)
+
+        ly_relative = tf.vectorized_map(light_yield_relative, elems=[ly_relative_energies_keV,
+                                                                     tf.ones_like(ly_relative_energies_keV) * eta_eh,
+                                                                     tf.ones_like(ly_relative_energies_keV) * dEdx_birks])
+        ly_relative = ly_relative / light_yield_relative([450., eta_eh, dEdx_birks])
+
+        return ly_relative
 
     final_dimensions = ('integrated_charge',)
     no_step_dimensions = ()
