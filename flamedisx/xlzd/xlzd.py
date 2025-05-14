6642bc91427581794f5c875f8a39d679606d5b33
Robert James <robert.james.19@ucl.ac.uk>
2025-05-14 14:34:27 +1000

    Back to using ROI definitions in acceptance, for correct mu. Fix to quantities that need re-calculating after overriding detector parameters.

--- flamedisx/xlzd/xlzd.py
+++ flamedisx/xlzd/xlzd.py
@@ -23,6 +23,7 @@ class XLZDSource:
                  drift_field_V_cm=80., gas_field_kV_cm=7.5, elife_ns=10000e3, g1=0.31,
                  temperature_K=174.1, pressure_bar=1.79, num_pmts=902, double_pe_fraction=0.2,
                  g1_gas=0.1, s2Fano=2., spe_res=0.38, spe_thr=0.375, spe_eff=1.,
+                 cS1_min=0., cS1_max=100., log10_cS2_min=2.5, log10_cS2_max=4.,
                  s2_thr=198., coin_level=4,
                  ignore_maps_acc=False, **kwargs):
         super().__init__(*args, **kwargs)
@@ -67,16 +68,25 @@ class XLZDSource:
             self.temperature, self.pressure)
         self.drift_velocity = fd_nest.calculate_drift_velocity(
             self.drift_field, self.density, self.temperature)
+        self.Wq_keV, self.alpha = fd_nest.calculate_work(self.density)
         self.extraction_eff = fd_nest.calculate_extraction_eff(self.gas_field, self.temperature)
+        self.s1_mean_mult = fd_nest.calculate_s1_mean_mult(self.spe_res)
         self.g2 = fd_nest.calculate_g2(self.gas_field, self.density_gas, self.gas_gap,
                                        self.g1_gas, self.extraction_eff)
 
+
+        self.cS1_min = cS1_min
+        self.cS1_max = cS1_max
+        self.log10_cS2_min = log10_cS2_min
+        self.log10_cS2_max = log10_cS2_max
         self.s2_thr = s2_thr
-        self.coin_level = coin_level
+        self.coin_table = fd_nest.get_coin_table(coin_level, self.num_pmts,
+                                                 self.spe_res, self.spe_thr, self.spe_eff,
+                                                 self.double_pe_fraction)
 
     def s1_acceptance(self, s1, cs1):
 
-        acceptance = tf.where(s1 >= self.spe_thr,
+        acceptance = tf.where((s1 >= self.spe_thr) & (cs1 >= self.cS1_min) & (cs1 <= self.cS1_max),
                               tf.ones_like(s1, dtype=fd.float_type()),  # if condition non-zero
                               tf.zeros_like(s1, dtype=fd.float_type()))  # if false
 
@@ -86,7 +96,8 @@ class XLZDSource:
 
         log10_cs2 = np.log10(cs2 + 1e-10)
 
-        acceptance = tf.where(s2 >= self.s2_thr,
+        acceptance = tf.where((s2 >= self.s2_thr) &
+                              (log10_cs2 >= self.log10_cS2_min) & (log10_cs2 <= self.log10_cS2_max),
                               tf.ones_like(s2, dtype=fd.float_type()),  # if condition non-zero
                               tf.zeros_like(s2, dtype=fd.float_type()))  # if false
 
