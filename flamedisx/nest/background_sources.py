873aeaff8192662de55e052e6eb3eba68c5a4c7f
Robert James <robert.james.19@ucl.ac.uk>
2025-06-18 22:40:17 +1000

    Update background spectra: normalisation now built in; no longer assuming flat spectra for Pb214 and Kr85; improvements to the SURF vs. LNGS atmospheric flux scaling; extend ER spectra to higher energies.

--- flamedisx/nest/background_sources.py
+++ flamedisx/nest/background_sources.py
@@ -22,18 +22,17 @@ export, __all__ = fd.exporter()
 class vERSource(fd_nest.nestERSource):
     """ER background source from solar neutrinos (PP + 7Be + CNO).
     Reads in energy spectrum from .pkl file, generated with LZ's DMCalc.
-    Normalise such that the spectrum predicts 77.467 events in 1 tonne year.
     """
 
     def __init__(self, *args, fid_mass=1., livetime=1., **kwargs):
         if ('detector' not in kwargs):
             kwargs['detector'] = 'default'
 
-        df_vER = pd.read_pickle(os.path.join(os.path.dirname(__file__), 'background_spectra/vER_spectrum.pkl'))
+        df = pd.read_pickle(os.path.join(os.path.dirname(__file__), 'background_spectra/vER_spectrum.pkl'))
 
-        self.energies = tf.convert_to_tensor(df_vER['energy_keV'].values, dtype=fd.float_type())
-        scale = fid_mass * livetime * 77.467
-        self.rates_vs_energy = tf.convert_to_tensor(df_vER['spectrum_value_norm'].values * scale, dtype=fd.float_type())
+        self.energies = tf.convert_to_tensor(df['energy_keV'].values, dtype=fd.float_type())
+        scale = fid_mass * livetime
+        self.rates_vs_energy = tf.convert_to_tensor(df['spectrum_value_norm'].values * scale, dtype=fd.float_type())
 
         super().__init__(*args, **kwargs)
 
@@ -42,18 +41,17 @@ class vERSource(fd_nest.nestERSource):
 class Xe136Source(fd_nest.nestERSource):
     """"Beta background source from the 2-neutrino double beta decay of 136Xe.
     Reads in energy spectrum from .pkl file.
-    Normalise such that the spectrum predicts 86.424 events in 1 tonne year.
     """
 
     def __init__(self, *args, fid_mass=1., livetime=1., **kwargs):
         if ('detector' not in kwargs):
             kwargs['detector'] = 'default'
 
-        df_vER = pd.read_pickle(os.path.join(os.path.dirname(__file__), 'background_spectra/136Xe_spectrum.pkl'))
+        df = pd.read_pickle(os.path.join(os.path.dirname(__file__), 'background_spectra/136Xe_spectrum.pkl'))
 
-        self.energies = tf.convert_to_tensor(df_vER['energy_keV'].values, dtype=fd.float_type())
-        scale = fid_mass * livetime * 86.424
-        self.rates_vs_energy = tf.convert_to_tensor(df_vER['spectrum_value_norm'].values * scale, dtype=fd.float_type())
+        self.energies = tf.convert_to_tensor(df['energy_keV'].values, dtype=fd.float_type())
+        scale = fid_mass * livetime
+        self.rates_vs_energy = tf.convert_to_tensor(df['spectrum_value_norm'].values * scale, dtype=fd.float_type())
 
         super().__init__(*args, **kwargs)
 
@@ -62,18 +60,17 @@ class Xe136Source(fd_nest.nestERSource):
 class Xe124Source(fd_nest.nestERSource):
     """"EC background source from the 2-neutrino double electron capture of 124Xe.
     Reads in energy spectrum from .pkl file.
-    Normalise such that the spectrum predicts 8.805 events in 1 tonne year.
     """
 
     def __init__(self, *args, fid_mass=1., livetime=1., **kwargs):
         if ('detector' not in kwargs):
             kwargs['detector'] = 'default'
 
-        df_vER = pd.read_pickle(os.path.join(os.path.dirname(__file__), 'background_spectra/124Xe_spectrum.pkl'))
+        df = pd.read_pickle(os.path.join(os.path.dirname(__file__), 'background_spectra/124Xe_spectrum.pkl'))
 
-        self.energies = tf.convert_to_tensor(df_vER['energy_keV'].values, dtype=fd.float_type())
-        scale = fid_mass * livetime * 8.805
-        self.rates_vs_energy = tf.convert_to_tensor(df_vER['spectrum_value_norm'].values * scale, dtype=fd.float_type())
+        self.energies = tf.convert_to_tensor(df['energy_keV'].values, dtype=fd.float_type())
+        scale = fid_mass * livetime
+        self.rates_vs_energy = tf.convert_to_tensor(df['spectrum_value_norm'].values * scale, dtype=fd.float_type())
 
         super().__init__(*args, **kwargs)
 
@@ -81,17 +78,19 @@ class Xe124Source(fd_nest.nestERSource):
 @export
 class Pb214Source(fd_nest.nestERSource):
     """Beta background source from 214Pb.
-    Normalise such that the spectrum predicts 11.41 events in 1 tonne year (0.1 mBq/kg).
+    Reads in energy spectrum from .pkl file.
+    Normalised to 0.1 mBq/kg.
     """
 
     def __init__(self, *args, fid_mass=1., livetime=1., activity_mBq_kg=0.1, **kwargs):
         if ('detector' not in kwargs):
             kwargs['detector'] = 'default'
 
-        self.energies = tf.cast(np.arange(0.4, 33.01, 0.1), fd.float_type())
-        self.rates_vs_energy = tf.ones_like(self.energies, fd.float_type()) / sum(np.ones_like(self.energies))
-        scale = fid_mass * livetime * (activity_mBq_kg / 0.1) *  11.41
-        self.rates_vs_energy = self.rates_vs_energy * tf.cast(scale, fd.float_type())
+        df = pd.read_pickle(os.path.join(os.path.dirname(__file__), 'background_spectra/214Pb_spectrum.pkl'))
+
+        self.energies = tf.convert_to_tensor(df['energy_keV'].values, dtype=fd.float_type())
+        scale = fid_mass * livetime * (activity_mBq_kg / 0.1)
+        self.rates_vs_energy = tf.convert_to_tensor(df['spectrum_value_norm'].values * scale, dtype=fd.float_type())
 
         super().__init__(*args, **kwargs)
 
@@ -99,17 +98,19 @@ class Pb214Source(fd_nest.nestERSource):
 @export
 class Kr85Source(fd_nest.nestERSource):
     """Beta background source from 85Kr.
-    Normalise such that the spectrum predicts 46.944 events in 1 tonne year (0.1 ppt).
+    Reads in energy spectrum from .pkl file.
+    Normalised to 0.1 ppt.
     """
 
     def __init__(self, *args, fid_mass=1., livetime=1., activity_ppt=0.1, **kwargs):
         if ('detector' not in kwargs):
             kwargs['detector'] = 'default'
 
-        self.energies = tf.cast(np.arange(0.4, 33.01, 0.1), fd.float_type())
-        self.rates_vs_energy = tf.ones_like(self.energies, fd.float_type()) / sum(np.ones_like(self.energies))
-        scale = fid_mass * (activity_ppt / 0.1) * livetime * 46.944
-        self.rates_vs_energy = self.rates_vs_energy * tf.cast(scale, fd.float_type())
+        df = pd.read_pickle(os.path.join(os.path.dirname(__file__), 'background_spectra/85Kr_spectrum.pkl'))
+
+        self.energies = tf.convert_to_tensor(df['energy_keV'].values, dtype=fd.float_type())
+        scale = fid_mass * livetime * (activity_ppt / 0.1)
+        self.rates_vs_energy = tf.convert_to_tensor(df['spectrum_value_norm'].values * scale, dtype=fd.float_type())
 
         super().__init__(*args, **kwargs)
 
@@ -118,7 +119,6 @@ class Kr85Source(fd_nest.nestERSource):
 class vNRSolarSource(fd_nest.nestNRSource):
     """CEvNS background source from B8 + HEP neutrinos.
     Reads in energy spectrum from .pkl file, generated with LZ's DMCalc.
-    Normalise such that the spectrum predicts 246.345 events in 1 tonne year.
     """
 
     def __init__(self, *args, fid_mass=1., livetime=1., **kwargs):
@@ -128,7 +128,7 @@ class vNRSolarSource(fd_nest.nestNRSource):
         df_CEvNS_solar = pd.read_pickle(os.path.join(os.path.dirname(__file__), 'background_spectra/CEvNS_solar_spectrum.pkl'))
 
         self.energies = tf.convert_to_tensor(df_CEvNS_solar['energy_keV'].values, dtype=fd.float_type())
-        scale = fid_mass * livetime * 246.345
+        scale = fid_mass * livetime
         self.rates_vs_energy = tf.convert_to_tensor(df_CEvNS_solar['spectrum_value_norm'].values * scale, dtype=fd.float_type())
 
         super().__init__(*args, **kwargs)
@@ -138,17 +138,16 @@ class vNRSolarSource(fd_nest.nestNRSource):
 class vNROtherLNGSSource(fd_nest.nestNRSource):
     """CEvNS background source from Atmospheric (LNGS flux) + DSNB neutrinos.
     Reads in energy spectrum from .pkl file, generated with LZ's DMCalc.
-    Normalise such that the spectrum predicts 0.071 events in 1 tonne year.
     """
 
     def __init__(self, *args, fid_mass=1., livetime=1., **kwargs):
         if ('detector' not in kwargs):
             kwargs['detector'] = 'default'
 
-        df_CEvNS_other = pd.read_pickle(os.path.join(os.path.dirname(__file__), 'background_spectra/CEvNS_other_spectrum.pkl'))
+        df_CEvNS_other = pd.read_pickle(os.path.join(os.path.dirname(__file__), 'background_spectra/CEvNS_other_LNGS_spectrum.pkl'))
 
         self.energies = tf.convert_to_tensor(df_CEvNS_other['energy_keV'].values, dtype=fd.float_type())
-        scale = fid_mass * livetime * 0.071
+        scale = fid_mass * livetime
         self.rates_vs_energy = tf.convert_to_tensor(df_CEvNS_other['spectrum_value_norm'].values * scale, dtype=fd.float_type())
 
         super().__init__(*args, **kwargs)
@@ -158,17 +157,16 @@ class vNROtherLNGSSource(fd_nest.nestNRSource):
 class vNROtherSURFSource(fd_nest.nestNRSource):
     """CEvNS background source from Atmospheric (SURF flux) + DSNB neutrinos.
     Reads in energy spectrum from .pkl file, generated with LZ's DMCalc.
-    Normalise such that the spectrum predicts 0.094 events in 1 tonne year.
     """
 
     def __init__(self, *args, fid_mass=1., livetime=1., **kwargs):
         if ('detector' not in kwargs):
             kwargs['detector'] = 'default'
 
-        df_CEvNS_other = pd.read_pickle(os.path.join(os.path.dirname(__file__), 'background_spectra/CEvNS_other_spectrum.pkl'))
+        df_CEvNS_other = pd.read_pickle(os.path.join(os.path.dirname(__file__), 'background_spectra/CEvNS_other_SURF_spectrum.pkl'))
 
         self.energies = tf.convert_to_tensor(df_CEvNS_other['energy_keV'].values, dtype=fd.float_type())
-        scale = fid_mass * livetime * 0.094
+        scale = fid_mass * livetime
         self.rates_vs_energy = tf.convert_to_tensor(df_CEvNS_other['spectrum_value_norm'].values * scale, dtype=fd.float_type())
 
         super().__init__(*args, **kwargs)
