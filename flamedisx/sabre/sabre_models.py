8b8df4bdc14112bc806d3a18a8bf34c78b2bf9a7
Robert James <robert.james.19@ucl.ac.uk>
2025-08-26 18:36:11 +1000

    Non-TF implementation of beta non-linearity model, to makea adding gamma model easier.

--- flamedisx/sabre/sabre_models.py
+++ flamedisx/sabre/sabre_models.py
@@ -4,11 +4,14 @@ import tensorflow_probability as tfp
 import flamedisx as fd
 from .. import sabre as fd_sabre
 
+import numpy as np
+from scipy import integrate
+
 export, __all__ = fd.exporter()
 
 
 @export
-class SABRESource(fd.BlockModelSource):
+class SABREBetaSource(fd.BlockModelSource):
     model_blocks = (
         fd_sabre.FixedShapeEnergySpectrum,
         fd_sabre.PhotonsPhotoelectrons,
@@ -18,65 +21,72 @@ class SABRESource(fd.BlockModelSource):
         self.energies= tf.cast(energies, dtype=fd.float_type())
         self.rates_vs_energy = tf.cast(rates_vs_energy, dtype=fd.float_type())
 
+        self.ly_relative_energies_keV = tf.cast(np.geomspace(5., 75., 100),
+                                                dtype=fd.float_type())
+        self.ly_relative = tf.cast(self.light_yield_relative_interp(self.ly_relative_energies_keV),
+                                   dtype=fd.float_type())
+
         super().__init__(*args, **kwargs)
 
     def eff_light_yield(self, energy, *, eff_ly=11.25):
         """
         """
-        ly_relative_energies_keV = tf.experimental.numpy.geomspace(1., 450., num=100, dtype=fd.float_type())
-        ly_relative = self.light_yield_relative_interp(ly_relative_energies_keV)
-
-        ly_relative_interp = tfp.math.interp_regular_1d_grid(energy, 1., 450., ly_relative)
+        ly_relative_interp = tfp.math.interp_regular_1d_grid(energy, self.ly_relative_energies_keV[0],
+                                                             self.ly_relative_energies_keV[-1], self.ly_relative)
 
         return eff_ly * ly_relative_interp
 
     def light_yield_relative_interp(self, ly_relative_energies_keV):
         """
         """
-        def light_yield_relative(args):
-            #: Fixed model parameters: materials
-            Z_NaI = 64
-            M_NaI = 149.89
-            rho_NaI = 3.67 # g / cm^3
-            I_keV_NaI = 0.452 # keV
+        #: Fixed model parameters: materials
+        Z_NaI = 64
+        M_NaI = 149.89
+        rho_NaI = 3.67 # g / cm^3
+        I_keV_NaI = 0.452 # keV
 
-            # Fixed parameters: model
-            c = 2.8
-            dEdx_ons = 36.4 # MeV / cm
-            eta_eh = 0.534
-            dEdx_birks = 166 # MeV / cm
+        # Fixed parameters: model
+        c = 2.8
+        dEdx_ons = 36.4 # MeV / cm
+        eta_eh = 0.534
+        dEdx_birks = 166 # MeV / cm
 
-            def eta_cap(E_keV):
-                dEdx = dEdx_MeV_cm(E_keV)
+        def dEdx_MeV_cm(E_keV):
+            E_ev = 1000. * E_keV
+            I_ev = 1000. * I_keV_NaI
 
-                numerator = 1. - eta_eh * tf.math.exp(-dEdx / dEdx_ons)
-                denominator = 1. + (dEdx / dEdx_birks)
+            prefactor = 785. * Z_NaI * rho_NaI / M_NaI
+            ln_arg = 1.16 * (E_ev + c * I_ev) / I_ev
 
-                return numerator / denominator
+            dEdx_ev_angstrom = prefactor * np.log(ln_arg) / E_ev
+            dEdx_MeV_cm = 1e-6 * 1e8 * dEdx_ev_angstrom
 
-            def dEdx_MeV_cm(E_keV):
-                E_ev = 1000. * E_keV
-                I_ev = 1000. * I_keV_NaI
+            return dEdx_MeV_cm
 
-                prefactor = 785. * Z_NaI * rho_NaI / M_NaI
-                ln_arg = 1.16 * (E_ev + c * I_ev) / I_ev
+        def eta_cap(E_keV):
+            dEdx = dEdx_MeV_cm(E_keV)
 
-                dEdx_ev_angstrom = prefactor * tf.math.log(ln_arg) / E_ev
-                dEdx_MeV_cm = 1e-6 * 1e8 * dEdx_ev_angstrom
+            numerator = 1. - eta_eh * np.exp(-dEdx / dEdx_ons)
+            denominator = 1. + (dEdx / dEdx_birks)
 
-                return dEdx_MeV_cm
+            return numerator / denominator
 
-            E_keV = args[0]
+        def light_yield(E_keV):
+            result = integrate.quad(lambda x: eta_cap(x) / (E_keV - I_keV_NaI), I_keV_NaI, E_keV)
 
-            x = tf.linspace(I_keV_NaI, E_keV, num=1000)
-            y = eta_cap(x)
+            return result[0]
 
-            return tfp.math.trapz(y, x=x) / (E_keV - I_keV_NaI)
+        vlight_yield = np.vectorize(light_yield)
 
-        ly_relative = tf.vectorized_map(light_yield_relative, elems=[ly_relative_energies_keV])
-        ly_relative = ly_relative / light_yield_relative([450.])
+        ly_relative = vlight_yield(ly_relative_energies_keV)
+        ly_relative = ly_relative / vlight_yield(46.5)
 
         return ly_relative
 
     final_dimensions = ('integrated_charge',)
     no_step_dimensions = ()
+
+
+@export
+class SABREGammaSource(SABREBetaSource):
+    pass
