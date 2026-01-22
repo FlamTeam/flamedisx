6d1a86a2bbc6d5e767d9effd9a4c3a51ac57246b
josh0-jrg <joshua.robert.green@gmail.com>
2026-01-22 11:16:04 +0000

    Fix: Allow original linear and new BSpline interpolation
    This isn't the best way, perhaps we could inherit and build. Wanted to keep it compatible with WS24 analysis in LZ

--- flamedisx/templates.py
+++ flamedisx/templates.py
@@ -212,26 +212,158 @@ class MultiTemplateSource(fd.Source):
             method = 'BSpline',
             *args,
             **kwargs):
-
+        """
+            Initialize the MultiTemplateSource that allows for template morphinh
+            Args:
+                - params_and_templates: 2-tuples of
+                        (dict of parameter names and values, template histogram).
+                        The parameter names must be the same for all templates.
+                - params_and_normalisations: 2-tuples of
+                        (dict of parameter names and values, normalisation factor).
+                        The parameter names must be the same for all normalisations.
+            Kwargs:
+                - bin_edges: None, or a list of numpy arrays with bin edges.
+                - axis_names: None, or a sequence of axis names.
+                - events_per_bin: set to True if templates specify expected events per bin,
+                                  rather than differential rate.
+                - interpolate: if True, differential rates are to be interpolated linearly
+                - _skip_tf_init: if True, skip tensorflow initialization (for subclassing).
+                - method: interpolation method between templates, either 'linear' or 'BSpline'.
+                          normalisations are always linaerly interpolated.
+        """      
         self._templates = [
             TemplateWrapper(
                 template, bin_edges, axis_names, events_per_bin, interpolate)
             for _, template in params_and_templates]
         assert method in ('linear','BSpline'), "Only 'linear' and 'BSpline' methods are supported"
         self._method = method
-        assert self._method == 'BSpline' and len(params_and_templates[0][0]) == 1, "BSpline only supports moprhing of 1 parameter"
-        assert self._method == 'linear', "Linear nD interpolation is not yet implemented"
+        if self._method == 'BSpline':
+            assert len(params_and_templates[0][0]) == 1, "BSpline only supports moprhing of 1 parameter"
         self.param_name = list(params_and_templates[0][0].keys())[0]
-
-        # We will include mu variation separately
-        self.mu = self._templates[0].mu
-
         # Grab parameter names. Promote first set of values to defaults.
         self.n_templates = n_templates = len(self._templates)
-        assert n_templates > 0
+        assert n_templates > 0, "Need at least one template to morph"
+        # We will include mu variation separately
+        self.mu = self._templates[0].mu
+        if params_and_normalisations is not None:
+            assert self.mu ==1, "If providing normalisations, template mu must be 1"
         defaults = params_and_templates[0][0]
         for params, _ in params_and_templates:
             assert tuple(params.keys()) == tuple(defaults.keys())
+
+        #where BSpline code diverges
+        if self._method == 'BSpline':
+            return self._init_BSpline( params_and_templates, params_and_normalisations, bin_edges,
+                axis_names, events_per_bin, interpolate, _skip_tf_init,
+                n_templates,
+                defaults,
+                *args,**kwargs)
+        elif self._method == 'linear':
+            return self._init_linear(params_and_templates, params_and_normalisations, bin_edges,
+                axis_names, events_per_bin, interpolate, _skip_tf_init,
+                n_templates,
+                defaults,
+                *args,**kwargs)
+        else:
+            raise NotImplementedError("Only 'linear' and 'BSpline' methods are supported, how did you get here?")
+
+    def _init_linear(self, params_and_templates, params_and_normalisations, bin_edges,
+                            axis_names, events_per_bin, interpolate, _skip_tf_init,
+                            n_templates,
+                            defaults,
+                            *args,**kwargs):
+        """
+            Initialize the original linear interpolation method. Works for many parameters, not C2 continuous.
+            TODO: implement params_and_normalisations support.
+            Args & Kwargs:
+                Most of the parameters are the same as for __init__.
+                Common parameters derived in __init__ are line seperate for clarity.
+        """
+        assert params_and_normalisations is None, "Normalisations not yet supported for linear interpolation"
+        # Build an interpolator that produces the _weights_ of each template
+        # at a given parameter space point, according to linear interpolation.
+        #
+        # This interpolator maps an (n_templates = n_params,) array to
+        # an (n_templates,) array.
+        #
+        # When evaluated at the exact location of a template, the result has 1
+        # in the corresponding template's position, and zeros elsewhere.
+        _template_weights = scipy.interpolate.LinearNDInterpolator(
+            points=np.asarray([list(params.values()) for params, _ in params_and_templates]),
+            values=np.eye(n_templates))
+
+        # Unfortunately TensorFlow has no equivalent of LinearNDInterpolator,
+        # only interpolators that work on rectilinear grids. Thus, instead of
+        # calling something like the above interpolator directly, we have to
+        # evaluate it on a rectilinear grid first. :-(
+
+        # Get the sorted unique values for each parameter, then use those
+        # to build a rectilinear grid. Tuple of differently-shaped arrays.
+        _grid_coordinates = tuple([
+            np.asarray(sorted(set(params[param]
+                                  for params, _ in params_and_templates)))
+            for param in defaults])
+        _full_grid_coordinates = np.meshgrid(*_grid_coordinates, indexing='ij')
+        n_grid_points = np.prod([len(x) for x in _grid_coordinates])
+
+        # Evaluate our irregular-grid scipy-interpolator on the grid.
+        # This gives an array of shape (n_templates, ngrid_dim0, ngrid_dim1, ...)
+        # for use in tensorflow interpolation.
+        _grid_weights = _template_weights(*_full_grid_coordinates)
+
+        # The expected number of events must also be interpolated.
+        # For consistency, it must be done in the same way (first interpolate
+        # to a regular grid, then linearly from there).
+        # (n_templates,) array
+        _template_mus = np.asarray([
+            template.mu for template in self._templates])
+        self._grid_mus = np.average(
+            # numpy won't let us get away with a size-1 axis here, we have to
+            # actually repeat the values. (If we had jax we could just vmap...)
+            np.repeat(_template_mus[:, None], n_grid_points, axis=1),
+            axis=0,
+            weights=_grid_weights.reshape(n_templates, n_grid_points))
+        assert self._grid_mus.shape == (n_templates,)
+        self._mu_interpolator = scipy.interpolate.RegularGridInterpolator(
+            points=_grid_coordinates,
+            values=self._grid_mus.reshape(_full_grid_coordinates[0].shape),
+            method='linear')
+
+        # Generate a random column name to use to store the diff rates
+        # of observed events under every template
+        self.column = (
+            'template_diff_rate_'
+            + ''.join(random.choices(string.ascii_lowercase, k=8)))
+
+        # ... this column will hold an array, with one entry per template
+        self.array_columns = ((self.column, n_templates),)
+
+        # This source has parameters but no model functions, so we can't do the
+        # usual Source.scan_model_functions.
+        self.f_dims = dict()
+        self.f_params = dict()
+        self.defaults = defaults
+
+        # This is needed in tensorflow, so convert it now
+        self._grid_coordinates = tuple([fd.np_to_tf(np.asarray(g)) for g in _grid_coordinates])
+        self._grid_weights = fd.np_to_tf(_grid_weights)
+        super().__init__(*args, **kwargs)
+        self.defaults = {**defaults,**{k: tf.cast(v, fd.float_type()) for k, v in defaults.items()}}
+        self.parameter_index = fd.index_lookup_dict(self.defaults.keys())
+        if not _skip_tf_init:
+            self.trace_differential_rate()
+
+    def _init_BSpline(self, params_and_templates, params_and_normalisations, bin_edges,
+                            axis_names, events_per_bin, interpolate, _skip_tf_init,
+                            n_templates,
+                            defaults,
+                            *args,**kwargs):
+        """
+            Initiliaztion the BSpline method, which works for 1 parameter only, but is C2 continuous.
+            Args & Kwargs:
+                Most of the parameters are the same as for __init__.
+                Common parameters derived in __init__ are line seperate for clarity.
+        """
 
         # Build an interpolator that produces the _weights_ of each template
         # at a given parameter space point, according to linear interpolation.
@@ -241,10 +373,11 @@ class MultiTemplateSource(fd.Source):
         #
         # When evaluated at the exact location of a template, the result has 1
         # in the corresponding template's position, and zeros elsewhere.
+
         _template_weights = scipy.interpolate.interp1d(
             x=np.asarray([list(params.values())[0] for params, _ in params_and_templates]),
             y=np.eye(n_templates))
-
+        
         # Unfortunately TensorFlow has no equivalent of LinearNDInterpolator,
         # only interpolators that work on rectilinear grids. Thus, instead of
         # calling something like the above interpolator directly, we have to
@@ -300,8 +433,7 @@ class MultiTemplateSource(fd.Source):
         self.array_columns = ((self.column, n_templates+8),)
 
         super().__init__(*args, **kwargs)
-
-        self.defaults = {**self.defaults,**{k: tf.cast(v, fd.float_type()) for k, v in defaults.items()}}
+        self.defaults = {**defaults,**{k: tf.cast(v, fd.float_type()) for k, v in defaults.items()}}
         self.parameter_index = fd.index_lookup_dict(self.defaults.keys())
         if not _skip_tf_init:
             self.trace_differential_rate()
@@ -312,6 +444,15 @@ class MultiTemplateSource(fd.Source):
     def _annotate(self):
         """Add columns needed in inference to self.data
         """
+        if self._method == 'linear':
+            # Get array of differential rates for each template.
+            # Outer list() is to placate pandas, which does not like array columns..
+            self.data[self.column] = list(np.asarray([
+                template.differential_rates_numpy(self.data)
+                for template in self._templates]).T)
+            return 0
+        elif self._method != 'BSpline':
+            raise NotImplementedError("Only 'linear' and 'BSpline' methods are supported for annotation")
         #construct tensor of knots
         #requires a tensor of elements
         #data is stored as [[d_evt1^h1,d_evt1^h2..],[d_evt2^h1,d_evt2^h2..]]
@@ -361,6 +502,12 @@ class MultiTemplateSource(fd.Source):
         return self.mu
 
     def estimate_mu(self, n_trials=None, **params):
+        if self._method == 'linear':
+            return self._mu_interpolator([
+                            params.get(param, default)
+                            for param, default in self.defaults.items()])
+        elif self._method != 'BSpline':
+            raise NotImplementedError("Only 'linear' and 'BSpline' methods are supported for mu estimation")
         norm = tfp.math.batch_interp_regular_1d_grid(
                     x=params[self.param_name],
                     x_ref_min=self.pmin,
@@ -375,14 +522,17 @@ class MultiTemplateSource(fd.Source):
             #second order non-cyclical b-spline with varying knots
             #returns [x,y] so ignore x
             #hackiest shit ever
+            shift = self.linear_shift*(param-self.linear_shift_shift)
+            knot_coord = self.max_pos*(param-self.start)/self.original_range+shift
             return tf.reduce_sum(bspline.interpolate(knots_for_event,
-                                                     self.max_pos*(param-self.start)/self.original_range+self.linear_shift*(param -self.linear_shift_shift), 2, False) \
+                                                     knot_coord,
+                                                        2, False) \
                                 * tf.constant([0,1],dtype=fd.float_type()))
         #vectorized map over all events
         y=tf.vectorized_map(interp,elems=knots)
         return y
 
-    def _differential_rate(self, data_tensor, ptensor):
+    def _differential_rate_BSpline(self, data_tensor, ptensor):
         norm = tfp.math.batch_interp_regular_1d_grid(
                 x=self._fetch_param(self.param_name, ptensor),
                 x_ref_min=self.pmin,
@@ -391,10 +541,45 @@ class MultiTemplateSource(fd.Source):
                 )
         
         knots_per_event=tf.convert_to_tensor([self.tensor_xvals, self._fetch(self.column, data_tensor)],dtype=fd.float_type())
-        bspline_diff_rates=self.bspline_interpolate_per_bin(self._fetch_param(self.param_name, ptensor), tf.transpose(knots_per_event,perm=[1,0,2]))
+        bspline_diff_rates=self.bspline_interpolate_per_bin(self._fetch_param(self.param_name, ptensor), 
+                                                            tf.transpose(knots_per_event,perm=[1,0,2]))
         dr=tf.squeeze(norm)*bspline_diff_rates
 
         return dr
+    
+    def _differential_rate_linear(self, data_tensor, ptensor):
+        # Compute template weights at this parameter point
+        # (n_templates,) tensor
+        # (The axis order is weird here. It seems to work...)
+        permutation = (
+            [self._grid_weights.ndim - 1]
+            + list(range(0, self._grid_weights.ndim - 1)))
+        template_weights = tfp.math.batch_interp_rectilinear_nd_grid(
+            x=ptensor[None, :],
+            x_grid_points=self._grid_coordinates,
+            y_ref=tf.transpose(self._grid_weights, permutation),
+            axis=1,
+        )[:, 0]
+        # Ensure template weights sum to one.
+        template_weights /= tf.reduce_sum(template_weights)
+
+        # Fetch precomputed diff rates for each template.
+        # (n_events, n_templates) tensor
+        template_diffrates = self._fetch(self.column, data_tensor)
+
+        # Compute weighted average of diff rates
+        # (n_events,) tensor
+        return tf.reduce_sum(
+            template_diffrates * template_weights[None, :],
+            axis=1)
+    
+    def _differential_rate(self, data_tensor, ptensor):
+        if self._method == 'linear':
+            return self._differential_rate_linear(data_tensor, ptensor)
+        elif self._method == 'BSpline':
+            return self._differential_rate_BSpline(data_tensor, ptensor)
+        else:
+            raise NotImplementedError("Only 'linear' and 'BSpline' methods are supported for differential rate estimation")
 
     def simulate(self, n_events, fix_truth=None, full_annotate=False,
                  keep_padding=False, **params):
